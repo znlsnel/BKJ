ba966318fc510ae0e633c8e1351539d3e3b8c73a
Udi <dnel0605@gmail.com>
2022-12-22 10:38:47 +0900

    1238_파티( dijkstra )

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,155 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
+int N, M, X;
+int MAX = 1000000000;
 
-enum MAXorMIN
+int vecCost[1001];
+int revCost[1001];
+
+vector<pair<int, int>> vec[10001];
+vector<pair<int, int>> rev[10001];
+
+void dijkstra(vector<pair<int, int>> v[], int cost[],  int start)
 {
-	// PQ객체 생성시 읽기 편하게 enum으로 만들었음
-	MIN = false,
-	MAX = true
-};
+	cost[start] = 0;
+	priority_queue<pair<int, int>> pq;
+	pq.push(make_pair(0, start));
 
-class PQ
-{
-public:
-	PQ(bool b)
+	while (!pq.empty())
 	{
-		isMAX = b; // true로 하면 maxHeap
-	}
+		int current = pq.top().second ;
+		int currDist = -pq.top().first;
+		pq.pop();
 
-	void push(int n)
-	{
-		v.push_back(n);
-		// vector을 사용했지만 배열로 해도 전혀 무방
-
-		int root = static_cast<int>(v.size()) - 1;
-		int c = root;
-
-		do
-		{ // 부모노드와 비교 후 자리 바꿈
-			root = max((c - 1) / 2, 0);
-			int rN = v[root];
-			int cN = v[c];
-
-			// minHeap, maxHeap을 고려해서 비교함수를 만듦
-			if (compare(rN, cN))
-			{
-				v[root] = cN;
-				v[c] = rN;
-			}
-
-			c = root;
-		} while (c > 0);
-	}
-
-	// minHeap과 maxHeap 사이에서 데이터 교환시 사용
-	void exChange(int n)
-	{
-		if (v.size() == 0) return;
-
-		int size = static_cast<int>(v.size()) - 1;
-
-		v[0] = n;
-
-		int c = 0;
-		int root = 0;
-
-		// 부모와 자식 비교 후 교환
-		do
+		if (cost[current] < currDist) continue;
+		for (int i = 0; i < v[current].size(); i++)
 		{
-			c = (root * 2) + 1;
-			if (c > size) break;
-
-			if (c < size && compare(v[c], v[c + 1]))
-				c++;
-
-			int rN = v[root];
-			int cN = v[c];
-
-
-			if (c <= size && compare(rN, cN))
+			int next = v[current][i].second - 1;
+			int nextDist = currDist + v[current][i].first;
+			if (cost[next] > nextDist)
 			{
-				v[root] = cN;
-				v[c] = rN;
-			}
-
-			root = c;
-
-		} while (root < size);
-
-
+				cost[next] = nextDist;
+				pq.push(make_pair(-cost[next], next));
+;			}
+		}
 	}
 
-	int top()
-	{
-		if ((int)v.size() == 0) return -1;
-		return v[0];
-	}
-
-	bool compare(int r, int c)
-	{
-		if (isMAX) return r < c; // maxHeap은 큰게 위로
-		return r > c; // minHeap은 작은게 위로
-	}
-
-	int size()
-	{
-		return static_cast<int>(v.size());
-	}
-
-	bool empty()
-	{
-		return static_cast<int>(v.size()) == 0;
-	}
-
-public:
-	bool isMAX = false;
-	vector<int> v;
-};
+}
 
 int main()
 {
-	ios::sync_with_stdio(false); // 속도 빨라지는 코드
-	cin.tie(NULL); // 속도 빨라지는 코드
+	cin >> N >> M >> X;
 
-	vector<int> v;
-	int N, n;
-	cin >> N;
+	fill(vecCost, vecCost + N, MAX);
+	fill(revCost, revCost + N, MAX);
 
-	PQ max = PQ(MAX);
-	PQ min = PQ(MIN);
-
-
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < M; i++)
 	{
-		cin >> n;
-		v.push_back(n);
+		int s, d, c;
+		cin >> s >> d >> c;
+		vec[s - 1].push_back(make_pair(c, d));
+		rev[d - 1].push_back(make_pair(c, s));
 	}
 
+	dijkstra(vec, vecCost, X - 1);
+	dijkstra(rev, revCost, X - 1);
 
 
+	int max = 0;
 	for (int i = 0; i < N; i++)
 	{
-		n = v[i];
-
-		if (max.size() == min.size())
-			max.push(n); // 작은 값을 내림차순 정렬
-		else
-			min.push(n); // 큰 값을 오름차순 정렬
-		// 그럼 maxHeap과 minHeap의 top값이 가운데 값이 됨
-
-		if (!max.empty() && !min.empty() && max.top() > min.top())
-		{
-			int maxN, minN;
-			maxN = max.top();
-			minN = min.top();
-
-			max.exChange(minN);
-			min.exChange(maxN);
-		}
-		cout << max.top() << '\n';
+		if (max < vecCost[i] + revCost[i])
+			max = vecCost[i] + revCost[i];
 	}
+
+	cout << max;
 
 	return 0;
 }
