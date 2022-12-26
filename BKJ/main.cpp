bc72e0af8591ba858a42571572f6bf2628589e00
Udi <dnel0605@gmail.com>
2022-12-26 11:30:10 +0900

    5719_거의 최단 경로 [ 경로 삭제, 경로 체크 ] (시간 초과)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <map>
+#include <list>
 using namespace std;
 
 int N, M, S, D;
@@ -10,15 +12,33 @@ int MAX = 1000000000;
 int cost[1001];
 int parent[1001];
 
-void dijkstra(vector<pair<int, int>> v[], int start)
+bool path[1001][1001];
+
+
+void addPath(int parent[])
 {
-	cost[start] = 0;
+	int current = D;
+	int pre = parent[D];
+
+	while (pre != MAX)
+	{
+		path[pre][current] = true;
+
+		int temp = pre;
+		pre = parent[pre];
+		current = temp;
+	}
+}
+
+void dijkstra(vector<pair<int, int>> v[])
+{
+	cost[S] = 0;
 	priority_queue<pair<int, int>> pq;
-	pq.push(make_pair(0, start));
+	pq.push(make_pair(0, S));
 
 	while (!pq.empty())
 	{
-		int currNode = pq.top().second ;
+		int currNode = pq.top().second;
 		int currDist = -pq.top().first;
 		pq.pop();
 
@@ -27,6 +47,8 @@ void dijkstra(vector<pair<int, int>> v[], int start)
 		{
 			int nextNode = v[currNode][i].second;
 			int nextDist = currDist + v[currNode][i].first;
+
+			if (path[currNode][nextNode])continue;
 			
 
 			if (cost[nextNode] > nextDist)
@@ -42,21 +64,40 @@ void dijkstra(vector<pair<int, int>> v[], int start)
 }
 
 
-
-void EraseEdge(vector<pair<int, int>> v[], int pos)
+// 굳이 지울필요가 있을까!?  --- 핵심 point
+void EraseEdge(vector<pair<int, int>> v[], int dest)
 {
-	int pre = parent[pos];
-	while (pre > MAX)
+	int end = dest;
+	int leastCost = cost[end];
+	int pre = parent[end];
+	int currCost = leastCost;
+
+	while (pre < MAX)
 	{
-		auto it = find_if(v[pre].begin(), v[pre].end(), [pos](pair<int, int> it) {return it.second == pos;});
+		int a = end;
+		auto it = find_if(v[pre].begin(), v[pre].end(), [a](pair<int, int> it) {return it.second == a;});
 
 		if (it == v[pre].end()) return;
-			
+
 		v[pre].erase(it);
 		int temp = pre;
 		pre = parent[pre];
-		pos = pre;
+		end = temp;
+
+		// TODO 
+		// vector의 경우 find_if 랑 erase의 속도가 O(N)이기 때문에 개선이 필요함
+		//  deque의 경우 erase는 괜찮지만 find_if는 안됨
+		
 	}
+
+	
+	fill(cost, cost + N, MAX);
+	dijkstra(v);
+	currCost = cost[dest];
+	
+	if (currCost == leastCost)
+		EraseEdge(v, dest);
+	
 }
 
 int main()
@@ -67,11 +108,14 @@ int main()
 		cin >> N >> M;
 		if (N == 0 && M == 0)
 			break;
-
+		
 		vector<pair<int, int>> v[10001];
+		
+		fill(parent, parent + N, MAX);
+		fill(cost, cost + N, MAX);
 
-		fill(parent, parent + 100, MAX);
-		fill(cost, cost + 100, MAX);
+		for (int i = 0; i < N; i++)
+			fill(path[i], path[i] + N, false);
 
 		cin >> S >> D;
 
@@ -81,15 +125,13 @@ int main()
 			cin >> s >> d >> c;
 			v[s].push_back(make_pair(c, d));
 		}
-		dijkstra(v, S);
-		EraseEdge(v, D);
 
-		fill(cost, cost + 100, MAX);
-		dijkstra(v, S);
+		dijkstra(v);
 
-		cout << cost[D] << "\n";
+		int result = cost[D] == MAX ? -1 : cost[D];
+		cout << result << "\n";
 	}
-	int a = 5;
+	
 
 
 	return 0;
