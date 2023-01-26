ba25ec6381a3a257123f7eb18a23e090bf571f5a
Udi <dnel0605@gmail.com>
2023-01-26 12:17:53 +0900

    2150 Strongly Connected Component

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,103 +1,90 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-
-// 아스키코드
-// 소문자 ( 97 ~ 122 )
-// 대문자 ( 65 ~ 90 )
-// 입력 받은 문자를 0 ~ 51까지의 index 번호로 바꿔줌
-#define chgId(char) char = char > 90 ? char - 71 : char - 65
-#define MAX  52
-
+#include <stack>
+#include <algorithm>
+#define MAX 10001
 using namespace std;
 
-// 모든 노드가 담길 vector
-vector<int> v[MAX];
+// 강한 결합 요소 ( SCC - Strongly Connected Component ) 알고리즘
 
-// 각 간선의 최대 유량 정보
-int limit[MAX][MAX];
+bool finished[MAX];		// SCC에 포함되어 있는 애들은 체크하여 중복 제거
+int seqID[MAX], id;		// 각 노드에 순서번호를 매겨서 root 노드를 체크함
+vector<int> v[MAX];		// 각 정점이 담길 그래프
+vector<vector<int>> SCC;	// SCC가 담길 vector
 
-// 각 간선의 현재 유량 정보
-int flow[MAX][MAX];
+stack<int> s;			// stack을 활용하여 scc 추출
 
-// 부모 노드 정보
-int parent[MAX];
-
-int MaxFlow()
+int dfs(int node)
 {
-	// A : 0,      Z : 25 
-	int result = 0, start = 0, end = 25;
+	int start = seqID[node] = ++id;
+	s.push(node);
 
-	while (true)
+	for (size_t i = 0; i < v[node].size(); i++)
 	{
-		// parent를 -1로 초기화
-		fill(parent, parent + MAX, -1);
-		queue<int> q;
-		q.push(start);
+		int next = v[node][i];
 
-		while (!q.empty())
-		{
-			int curr = q.front();
-			q.pop();
+		if (seqID[next] == 0)
+			start = min(start, dfs(next));
 
-			for (int i = 0; i < v[curr].size(); i++)
-			{
-				int next = v[curr][i];
-
-				// 흘려 보낼 공간이 남아있고 이미 검색된 노드가 아닐때
-				if (limit[curr][next] > flow[curr][next] && parent[next] == -1)
-				{
-					q.push(next);
-					// 부모노드 세팅
-					parent[next] = curr;
-				}
-			}
-		}
-	
-		// parent[end]가 -1이라는 것은 end로 흘려보낼 공간이 남아있지 않다는 것
-		// 즉 검색이 완료되었다는 것을 의미함
-		if (parent[end] == -1) return result;
-
-		// end로 가는 경로로 흘려 보낼 수 있는 용량을 구함
-		int min = 1000;
-		for (int i = end; i != start; i = parent[i])
-		{
-			// 각 간선의  최대 유량 - 현재 유량 = 남은 공간
-			int remain = limit[parent[i]][i] - flow[parent[i]][i];
-			if (min > remain) min = remain;
-		}
-
-		// 현재 흐르고 있는 양 체크
-		for (int i = end; i != start; i = parent[i])
-		{
-			flow[parent[i]][i] += min;
-			flow[i][parent[i]] -= min;
-		}
-		result += min;
+		else if (finished[next] == false)
+			start = min(start, seqID[next]);
 	}
 
+	if (start == seqID[node])
+	{
+
+		vector<int> scc;
+		while (true)
+		{
+			int _node = s.top();
+			s.pop();
+			finished[_node] = true;
+			scc.push_back(_node);
+
+			if (_node == node)
+				break;
+			
+		}
+
+		SCC.push_back(scc);
+	}
+	return start;
+}
+
+bool com(vector<int>& a, vector<int>& b)
+{
+	return a[0] < b[0];
 }
 
 int main()
 {
-	int N;
-	char start, dest;
-	int capacity;
-	cin >> N;
-	
-	for (int i = 0; i < N; i++)
+	int V, E;
+	cin >> V >> E;
+
+	int s, d;
+	for (int i = 0; i < E; i++)
 	{
-		cin >> start >> dest >> capacity;
-		chgId(start);
-		chgId(dest);
-
-		v[start].push_back(dest);
-		v[dest].push_back(start);
-
-		limit[start][dest] += capacity;
-		limit[dest][start] += capacity;
+		cin >> s >> d;
+		v[s].push_back(d);
 	}
-	cout << MaxFlow();
+	
+	for (int i = 1; i <= V; i++)
+	{
+		if (seqID[i] == 0) dfs(i);
+	}
+	
+	// 오름차순 정렬
+	for (int i = 0; i < SCC.size(); i++)
+		sort(SCC[i].begin(), SCC[i].end());
+	sort(SCC.begin(), SCC.end(), com);
+	
+	cout << SCC.size()<< "\n";
+	for (int i = 0; i < SCC.size(); i++)
+	{
+		for (int j = 0; j < SCC[i].size(); j++)
+			cout << SCC[i][j] << " ";
+		cout << "-1\n";
+	}
 
 	return 0;
 }
