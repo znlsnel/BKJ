06b9be2006bedadf52263be46b9914e0aac30f8a
Udi <dnel0605@gmail.com>
2023-01-27 11:26:19 +0900

    6543 그래프의 싱크(SCC)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,90 +1,100 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <stack>
-#include <algorithm>
-#define MAX 10001
+
 using namespace std;
+const int MAX = 5000;
 
-// 강한 결합 요소 ( SCC - Strongly Connected Component ) 알고리즘
+int n, m, id, CN, cycleN[MAX], seqN[MAX];
+vector<int> v[MAX];
+bool finished[MAX];
+stack<int> S;
+vector<vector<int>> SCC;
 
-bool finished[MAX];		// SCC에 포함되어 있는 애들은 체크하여 중복 제거
-int seqID[MAX], id;		// 각 노드에 순서번호를 매겨서 root 노드를 체크함
-vector<int> v[MAX];		// 각 정점이 담길 그래프
-vector<vector<int>> SCC;	// SCC가 담길 vector
+int DFS(int curr) {
+	seqN[curr] = ++id;
+	S.push(curr);
 
-stack<int> s;			// stack을 활용하여 scc 추출
-
-int dfs(int node)
-{
-	int start = seqID[node] = ++id;
-	s.push(node);
-
-	for (size_t i = 0; i < v[node].size(); i++)
+	int parent = seqN[curr];
+	for (int next : v[curr]) 
 	{
-		int next = v[node][i];
+		if (seqN[next] == 0) 
+			parent = min(parent, DFS(next));
 
-		if (seqID[next] == 0)
-			start = min(start, dfs(next));
-
-		else if (finished[next] == false)
-			start = min(start, seqID[next]);
+		else if (!finished[next]) 
+			parent = min(parent, seqN[next]);
 	}
 
-	if (start == seqID[node])
-	{
-
+	if (parent == seqN[curr]) {
 		vector<int> scc;
-		while (true)
-		{
-			int _node = s.top();
-			s.pop();
-			finished[_node] = true;
-			scc.push_back(_node);
-
-			if (_node == node)
-				break;
-			
+		while (1) {
+			int t = S.top();
+			S.pop();
+			scc.push_back(t);
+			finished[t] = true;
+			cycleN[t] = CN;
+			if (t == curr) break;
 		}
 
+		sort(scc.begin(), scc.end());
 		SCC.push_back(scc);
+		CN++;
 	}
-	return start;
+
+	return parent;
 }
 
-bool com(vector<int>& a, vector<int>& b)
-{
-	return a[0] < b[0];
-}
 
-int main()
-{
-	int V, E;
-	cin >> V >> E;
+int main() {
+	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
 
-	int s, d;
-	for (int i = 0; i < E; i++)
-	{
-		cin >> s >> d;
-		v[s].push_back(d);
-	}
-	
-	for (int i = 1; i <= V; i++)
-	{
-		if (seqID[i] == 0) dfs(i);
-	}
-	
-	// 오름차순 정렬
-	for (int i = 0; i < SCC.size(); i++)
-		sort(SCC[i].begin(), SCC[i].end());
-	sort(SCC.begin(), SCC.end(), com);
-	
-	cout << SCC.size()<< "\n";
-	for (int i = 0; i < SCC.size(); i++)
-	{
-		for (int j = 0; j < SCC[i].size(); j++)
-			cout << SCC[i][j] << " ";
-		cout << "-1\n";
-	}
+	while (1) {
+		cin >> n;
+		if (n == 0) break;
+		cin >> m;
 
+		id = 0;
+		SCC.clear();
+		CN = 0;
+		while (!S.empty()) S.pop();
+		for (int i = 0; i < n; ++i)	v[i].clear();
+
+		fill(finished, finished + MAX, false);
+		fill(seqN, seqN + MAX, 0);
+		fill(cycleN, cycleN + MAX, -1);
+
+
+		for (int i = 0; i < m; ++i) {
+			int a, b;
+			cin >> a >> b;
+			v[a - 1].push_back(b - 1);
+		}
+
+		for (int i = 0; i < n; ++i)
+			if (seqN[i] == 0) DFS(i);
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < v[i].size(); ++j) 
+			{
+				int currCycle = cycleN[i];
+				int nextNode = cycleN[v[i][j]];
+
+				// 다음 노드가 현재 사이클에 포함되지 않는다면
+				if (currCycle != nextNode) SCC[currCycle].clear();
+			}
+		}
+
+		sort(SCC.begin(), SCC.end());
+
+		for (int i = 0; i < SCC.size(); ++i) {
+			if (!SCC[i].empty()) {
+				for (int j = 0; j < SCC[i].size(); ++j)
+					cout << SCC[i][j] + 1 << ' ';
+			}
+		}
+		cout << '\n';
+
+	}
 	return 0;
 }
