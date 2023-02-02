12893fc4701e7420449a43acd31db74b824b1e74
Udi <dnel0605@gmail.com>
2023-02-02 10:31:12 +0900

    12891 DNA 비밀번호

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
+int Arr[4];
+int checkArr[4];
+int checkSecret = 0;
 
+void Add(char c);
+void Remove(char c);
 
 int main()
 {
@@ -11,35 +14,92 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-	vector<int> v(N, 0);
-
-	for (int i = 0; i < N; i++)
-		cin >> v[i];
-
-	sort(v.begin(), v.end());
+	int S, P;
 	
-	int i = 0;
-	int j = N - 1;
-	int sum;
-	int count = 0;
+	string A;
 
-	while (i < j)
+	cin >> S >> P;
+	cin >> A;
+
+	for (int i = 0; i < 4; i++)
 	{
-		sum = v[i] + v[j];
-
-		if (sum < M) 
-			i++;
-
-		else if (sum > M) 
-			j--;
-
-		else
-		{
-			count++;
-			i++;
-		}
+		cin >> checkArr[i];
+		if (checkArr[i] == 0)
+			checkSecret++;
 	}
-	cout << count;
+
+	for (int i = 0; i < P; i++)
+		Add(A[i]);
+
+	int result = 0;
+
+	if (checkSecret == 4)
+		result++;
+
+	for (int i = P; i < S; i++)
+	{
+		int j = i - P;
+		Add(A[i]);
+		Remove(A[j]);
+
+		if (checkSecret == 4)
+			result++;
+	}
+	cout << result;
+}
+
+void Add(char c)
+{
+	switch (c)
+	{
+	case 'A':
+		Arr[0]++;
+		if (Arr[0] == checkArr[0])
+			checkSecret++;
+		break;
+
+	case 'C':
+		Arr[1]++;
+		if (Arr[1] == checkArr[1])
+			checkSecret++;
+		break;
+	case 'G':
+		Arr[2]++;
+		if (Arr[2] == checkArr[2])
+			checkSecret++;
+		break;
+	case 'T':
+		Arr[3]++;
+		if (Arr[3] == checkArr[3])
+			checkSecret++;
+		break;
+	}
+}
+
+void Remove(char c)
+{
+	switch (c)
+	{
+	case 'A':
+		if (Arr[0] == checkArr[0])
+			checkSecret--;
+		Arr[0]--;
+		break;
+
+	case 'C':
+		if (Arr[1] == checkArr[1])
+			checkSecret--;
+		Arr[1]--;
+		break;
+	case 'G':
+		if (Arr[2] == checkArr[2])
+			checkSecret--;
+		Arr[2]--;
+		break;
+	case 'T':
+		if (Arr[3] == checkArr[3])
+			checkSecret--;
+		Arr[3]--;
+		break;
+	}
 }
