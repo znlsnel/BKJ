8583aaa68951f94acbdf90b7f7552cdfdbb84333
Udi <dnel0605@gmail.com>
2023-02-06 14:32:35 +0900

    17298 오큰수

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,51 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
 
-vector<int> v;
-
-void Solution()
-{
-	string str;
-	stack<int> s;
-	int id = 0;
-
-	for (int i = 1; i <= v.size(); i++)
-	{
-		s.push(i);
-		str.push_back('+');
-		
-		while (!s.empty() && v[id] == s.top())
-		{
-			s.pop();
-			str.push_back('-');
-			id++;
-		}
-	}
-
-	if (!s.empty())
-	{
-		cout << "NO";
-		return;
-	}
-
-	for (int i = 0; i < str.size(); i++)
-		cout << str[i] << "\n";
-	
-
-}
+int N;
+stack<int> s;
+int ans[1000001];
+int seq[1000001];
 
 int main()
 {
-	int N;
 	cin >> N;
-	v.resize(N, 0);
 
 	for (int i = 0; i < N; i++)
-		cin >> v[i];
-	
-	Solution();
+		cin >> seq[i];
 
-	return 0;
+	for (int i = N - 1; i >= 0; i--)
+	{
+		while (!s.empty() && seq[i] >= s.top())
+			s.pop();
+
+		if (s.empty())
+			ans[i] = -1;
+		else
+			ans[i] = s.top();
+
+		s.push(seq[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+		cout << ans[i] << " ";
 }
