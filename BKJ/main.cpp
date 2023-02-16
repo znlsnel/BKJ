1cebc424baf4e82c62ec9e164fef1bcd3cd50bc9
Udi <dnel0605@gmail.com>
2023-02-16 21:20:25 +0900

    1377 버블 소트

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,119 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-class minHeap
-{
-public:
-	void push(int n)
-	{
-		v.push_back(n);
-
-		int curr = v.size() - 1;
-		int parent = (curr - 1) / 2;
-
-		while (parent >= 0)
-		{
-			if (abs(v[curr]) >= abs(v[parent]))
-				break;
-
-			{ // 데이터 교환
-				int temp = v[curr];
-				v[curr] = v[parent];
-				v[parent] = temp;
-			}
-
-			curr = parent;
-			parent = (curr - 1) / 2;
-		}
-	}
-
-	void pop()
-	{
-		if (v.empty()) return;
-
-		int end = v.size() - 1;
-		v[0] = v[end--];
-		v.pop_back();
-
-		int curr = 0;
-		int lChild = curr * 2 + 1;
-		int rChild = lChild + 1;
-		int minChild;
-
-		while (lChild <= end)
-		{
-			if (rChild > end || abs(v[lChild]) < abs(v[rChild]))
-				minChild = lChild;
-			else minChild = rChild;
-
-			if (abs(v[curr]) <= abs(v[minChild]))
-				break;
-
-			{
-				int temp = v[curr];
-				v[curr] = v[minChild];
-				v[minChild] = temp;
-
-				curr = minChild;
-				lChild = curr * 2 + 1;
-				rChild = lChild + 1;
-			}
-		}
-	}
-
-	int top()
-	{
-		if (v.empty()) return INT32_MAX;
-		return v.front();
-	}
-
-	bool empty() { return v.empty(); }
-
-private:
-	vector<int> v;
-};
 
 int main()
 {
-	int N, x;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
 	cin >> N;
-	vector<int> result;
+	vector<pair<int, int>> A(N);
 
-	minHeap mh;
-	minHeap nMh;
-	while (N--)
+	for (int i = 0; i < N; i++)
 	{
-
-		cin >> x;
-		if (x == 0)
-		{
-			if (mh.empty() && nMh.empty())
-			{
-				result.push_back(0);
-				continue;
-			}
-
-			minHeap* temp;
-			if (mh.top() < abs(nMh.top()))
-				temp = &mh;
-			else
-				temp = &nMh;
-
-			result.push_back(temp->top());
-			temp->pop();
-
-		}
-		else
-		{
-			if (x > 0)
-				mh.push(x);
-			else
-				nMh.push(x);
-
-		}
-
+		cin >> A[i].first;
+		A[i].second = i;
 	}
-	for (int i = 0; i < result.size(); i++)
-		cout << result[i] << "\n";
+
+	sort(A.begin(), A.end());
+	int Max = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (Max < A[i].second - i)
+			Max = A[i].second - i;
+	}
+	cout << Max + 1;
+	return 0;
 }
