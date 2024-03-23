f6f32c67de8147659a0d239d01214b14947b98cb
Udi <dnel0605@gmail.com>
2024-03-24 01:28:43 +0900

    2178 미로 탐색 (BFS)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -4,91 +4,55 @@
 
 using namespace std;
 
-vector<vector<int>>  _map;
-
-int _destX, _destY;
-struct POS
-{
-	POS() {
-		x = -1; y = -1; isinRange = true; distance = 0;
-	}
-	POS(int posX, int posY, int dst = 0) {
-		x = posX;
-		y = posY;
-		distance = dst; 
-		if (x > _destX || y > _destY || x < 0 || y < 0) {
-			isinRange = false; 
-		}
-		else
-			isinRange = true;
-	}   
-	  
-	bool operator !=(POS other) {
-		return this->x != other.x || this->y != other.y;		 
-	}
-	  
-	bool isinRange;
-	int x;
-	int y;
-
-	int distance;
+struct POS {
+        int y, x;
 };
-vector<vector<bool>>  _visit;
- 
-int _count = 20;
-int _minDistance = 100;
-void DFS(POS cur)
-{
-	if (cur.x == _destX && cur.y == _destY)
-	{ 
-		if (_minDistance > cur.distance) {
-			_minDistance = cur.distance; 
-		} 
-	}
-	  
-	POS downPos = POS(cur.x, cur.y + 1, cur.distance + 1);
-	POS upPos = POS(cur.x, cur.y - 1, cur.distance + 1);
-	POS leftPos = POS(cur.x - 1, cur.y, cur.distance + 1);
-	POS rightPos = POS(cur.x + 1, cur.y, cur.distance + 1);
-	  
-	auto checkCanMove = [&](POS nextPos)
-	{
-		bool canMove = nextPos.isinRange && _map[nextPos.y][nextPos.x] && _visit[nextPos.y][nextPos.x] == false;
-		if (canMove) 
-		{ 
-			_visit[nextPos.y][nextPos.x] = true;   
-			DFS(nextPos);
-			_visit[nextPos.y][nextPos.x] = false;
-		}  
-	};
-	  
-	checkCanMove(upPos); 
-	checkCanMove(downPos); 
-	checkCanMove(leftPos);
-	checkCanMove(rightPos); 
-}
 
-int main()
-{
-	cin >> _destY;
-	cin >> _destX;
-	_map.resize(_destY, vector<int>(_destX, 0));
-	_visit.resize(_destY, vector<bool>(_destX, false));
-	std::string line;
+int main() {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> maze(N, vector<int>(M));
+        vector<vector<bool>> visited(N, vector<bool>(M, false));
+        string line;
 
-	for (int i = 0; i < _destY; ++i) {
-		// 한 줄씩 문자열로 입력 받음
-		std::cin >> line; 
-		for (int j = 0; j < _destX; ++j) {
-			// 문자열의 각 문자를 정수로 변환하여 저장
-			_map[i][j] = line[j] - '0';
-		}
-	} 
+        for (int i = 0; i < N; ++i) {
+                cin >> line;
+                for (int j = 0; j < M; ++j) {
+                        maze[i][j] = line[j] - '0';
+                }
+        }
 
-	_destX--;
-	_destY--;
-	_visit[0][0] = true;
-	DFS(POS(0, 0));
-	
-	cout << _minDistance + 1; 
+        queue<POS> q;
+        q.push({ 0, 0 });  // 시작 위치
+        visited[0][0] = true;
+        int steps = 1;  // 시작 위치도 포함하기 때문에 1부터 시작
+
+        // 방향 이동: 상, 하, 좌, 우
+        int dx[4] = { 1, -1, 0, 0 };
+        int dy[4] = { 0, 0, 1, -1 };
+
+        while (!q.empty()) {
+                int size = q.size();
+                for (int i = 0; i < size; ++i) {
+                        POS cur = q.front();
+                        q.pop();
+
+                        if (cur.y == N - 1 && cur.x == M - 1) {  // 도착 지점에 도달
+                                cout << steps;
+                                return 0;
+                        }
+
+                        for (int j = 0; j < 4; ++j) {
+                                int nx = cur.x + dx[j];
+                                int ny = cur.y + dy[j];
+
+                                // 범위 체크 및 방문 체크
+                                if (nx >= 0 && nx < M && ny >= 0 && ny < N && maze[ny][nx] == 1 && !visited[ny][nx]) {
+                                        visited[ny][nx] = true;
+                                        q.push({ ny, nx });
+                                }
+                        }
+                }
+                ++steps;  // 다음 거리로 이동
+        }
 }
