5c63d62b5f26083d0141b0031cdadceeff861d1d
Udi <dnel0605@gmail.com>
2024-03-27 22:32:52 +0900

    1929 소수 구하기 (에라토스테네스의 체) [실버3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -14,46 +14,29 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 	 
-	int answer = 0;
-	string example;
-	cin >> example; 
-	vector<string> str1 = split(example, '-');
+	int M , N;
+	cin >> M >> N;
 
-	for (size_t i = 0; i < str1.size(); i++)
-	{
-		int n = 0;
-		vector<string> str2 = split(str1[i], '+');
-		for (size_t j = 0; j < str2.size(); j++)
-		{
-			for (size_t k = 0; k < str2[j].size(); k++)
-			{ 
-				n += (str2[j][k] - '0') * pow(10, str2[j].size() - 1 - k); 
-			}
-		} 
-		if (i == 0)
-			answer += n;
-		else
-			answer -= n;
-	}
-	cout << answer << "\n"; 
-}
-  
-vector<string> split(string example, char s)
-{
-	vector<string> result; 
-	
-	string str;
-	for (size_t i = 0; i < example.size(); i++)
-	{
-		if (example[i] != s)
-			str.push_back(example[i]);
-		else
-		{
-			result.push_back(str);
-			str = "";
+	vector<int> A(N + 1); 
+	 
+	for (int i = 2; i <= N; i++)
+		A[i] = i; 
+	 
+	for (int i = 2; i <= sqrt(N); i++) {
+		if (A[i] == 0) 
+			continue;
+		 
+		for (int j = i + i; j <= N; j += i) {
+			A[j] = 0; 
 		}
-	} 
-	result.push_back(str);
+	}
+	 
 
-	return result;
+	for (int i = M; i <= N; i++) {
+		if (A[i] != 0) 
+			cout << A[i] << "\n";
+	}
+	
+
+
 }
