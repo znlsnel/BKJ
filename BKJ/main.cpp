d964942aee0f8c17ad2809dd1492a50640417c12
Udi <dnel0605@gmail.com>
2024-03-27 20:18:20 +0900

    1931 회의실 배정 (그리디) [실버 1]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -14,62 +14,26 @@ int main()
 
 	long N;
 	cin >> N; 
-	priority_queue<int, vector<int>> p_pq;
-	priority_queue<int, vector<int>, greater<int>> n_pq;
-	int data;
-	int zeroCount = 0;
-	int oneCount = 0;
-	int sum = 0;
+	vector<pair<int, int>> times(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> times[i].second;
+		cin >> times[i].first;
+	} 
+
+	sort(times.begin(), times.end());
+
+	int count = 0;
+	int end = -1;
 
 	for (int i = 0; i < N; i++)
 	{
-		cin >> data;
-		if (data > 1)
-			p_pq.push(data);
-		else if (data == 0)
-			zeroCount++;
-		else if (data == 1)
-			oneCount++;
-		else 
-			n_pq.push(data); 
-	}
-
-	int data1 = 0;
-	int data2 = 0;
-
-	while (p_pq.size() > 1)
-	{
-		data1 = p_pq.top();
-		p_pq.pop();
-		data2 = p_pq.top();
-		p_pq.pop();
-		sum += data1 * data2;
-	}   
-	if (p_pq.size()) {
-		sum += p_pq.top();
-		p_pq.pop();
-	}
-
-	while (n_pq.size() > 1)
-	{
-		data1 = n_pq.top();
-		n_pq.pop();
-		data2 = n_pq.top();
-		n_pq.pop();
-		sum += data1 * data2;
-	} 
-	if (n_pq.size()) {
-		if (zeroCount == 0)
-		{
-			sum += n_pq.top();
-			n_pq.pop(); 
-
+		if (times[i].second >= end) {
+			end = times[i].first; 
+			count++;
 		}
 	}
-	 
-
-	sum += oneCount;
-	cout << sum << "\n";
+	cout << count << "\n"; 
 
 }
  
