a8ceb17cdf8da2fd82ee4cb4d4c654d567e9ca6e
Udi <dnel0605@gmail.com>
2024-03-27 21:47:28 +0900

    1541 잃어버린 괄호 (그리디) [실버 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -2,38 +2,58 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <cmath>
 
 using namespace std;
 
-int main() 
+vector<string> split(string example, char s);
+
+int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	 
+	int answer = 0;
+	string example;
+	cin >> example; 
+	vector<string> str1 = split(example, '-');
 
-	long N;
-	cin >> N; 
-	vector<pair<int, int>> times(N);
-
-	for (int i = 0; i < N; i++) {
-		cin >> times[i].second;
-		cin >> times[i].first;
-	} 
-
-	sort(times.begin(), times.end());
-
-	int count = 0;
-	int end = -1;
-
-	for (int i = 0; i < N; i++)
+	for (size_t i = 0; i < str1.size(); i++)
 	{
-		if (times[i].second >= end) {
-			end = times[i].first; 
-			count++;
-		}
+		int n = 0;
+		vector<string> str2 = split(str1[i], '+');
+		for (size_t j = 0; j < str2.size(); j++)
+		{
+			for (size_t k = 0; k < str2[j].size(); k++)
+			{ 
+				n += (str2[j][k] - '0') * pow(10, str2[j].size() - 1 - k); 
+			}
+		} 
+		if (i == 0)
+			answer += n;
+		else
+			answer -= n;
 	}
-	cout << count << "\n"; 
-
+	cout << answer << "\n"; 
 }
- 
+  
+vector<string> split(string example, char s)
+{
+	vector<string> result; 
+	
+	string str;
+	for (size_t i = 0; i < example.size(); i++)
+	{
+		if (example[i] != s)
+			str.push_back(example[i]);
+		else
+		{
+			result.push_back(str);
+			str = "";
+		}
+	} 
+	result.push_back(str);
+
+	return result;
+}
