140729ddc104bdbee6cc2eff88d7cc7d56b47002
Udi <dnel0605@gmail.com>
2024-03-28 11:22:17 +0900

    1456 거의 소수 (소수) [골드5]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -6,37 +6,43 @@
 
 using namespace std;
 
-vector<string> split(string example, char s);
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout.tie(NULL);
+	cout.tie(NULL); 
 	 
-	int M , N;
-	cin >> M >> N;
 
-	vector<int> A(N + 1); 
-	 
-	for (int i = 2; i <= N; i++)
-		A[i] = i; 
-	 
-	for (int i = 2; i <= sqrt(N); i++) {
-		if (A[i] == 0) 
-			continue;
+		long Min, Max;
+		cin >> Min >> Max;
+		long A[10000001];
+
+		for (int i = 2; i < 10000001; i++)
+			A[i] = i;
 		 
-		for (int j = i + i; j <= N; j += i) {
-			A[j] = 0; 
-		}
-	}
-	 
+		for (int i = 2; i <= sqrt(10000001); i++)
+		{
+			if (A[i] == 0)
+				continue;
 
-	for (int i = M; i <= N; i++) {
-		if (A[i] != 0) 
-			cout << A[i] << "\n";
-	}
+			for (int j = i + i; j < 10000001; j += i)
+				A[j] = 0;
+		} 
+		 
+		int count = 0;
+		for (int i = 2; i < 10000001; i++)
+		{
+			if (A[i] == 0) continue; 
+
+			long temp = A[i];
+			while ((double)A[i] <= (double)Max / (double)temp)
+			{
+				if (double(A[i] >= double(Min) / double(temp)))
+					count++;
+				temp = temp * A[i];  
+			}
+		} 
+
+		cout << count << "\n";
 	
-
-
 }
