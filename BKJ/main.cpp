432d4129340b25c51cc75635bd8d7ff8c209dff3
Udi <dnel0605@gmail.com>
2024-03-28 15:54:18 +0900

    11689 GCD(n, k) = 1 (오일러 피) [골드 1]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -14,28 +14,21 @@ int main()
 	cout.tie(NULL); 
 	 
 	// 2 3 5 6 7 8 10
-		long Min, Max;
-		cin >> Min >> Max;
-		// 2 - 10
-		vector<bool> Check(Max - Min + 1);
-		 
-		for (long i = 2; i * i <= Max; i++)
-		{
-			long pow = i * i; // 4
-			long start_index = Min / pow; // 0
+	long n;
+	cin >> n;
+	long result = n;
 
-			if (Min % pow != 0)
-				start_index++;
-
-			for (long j = start_index; pow * j <= Max; j++)
-				Check[int(j * pow - Min)] = true;
+	for (long p = 2; p <= sqrt(n); p++)
+	{
+		if (n % p == 0) {
+			result -= result / p;
+			while (n % p == 0)
+				n /= p;
 		}
+	}
 
-		int count = 0;
-		for (int i = 0; i <= Max - Min; i++)
-		{
-			if (!Check[i])
-				count++;
-		}
-		cout << count << "\n"; 
+	if (n > 1)
+		result -= result / n;
+
+	cout << result << "\n"; 
 }
