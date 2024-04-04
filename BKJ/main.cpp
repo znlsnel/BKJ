98467691459d73ffb9e8cae82c5e83dabe08bb20
Udi <dnel0605@gmail.com>
2024-04-04 09:48:47 +0900

    2252 줄 세우기 (위상정렬) [Gold III]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -2,101 +2,52 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include <cmath>
-#include <string>
-#include <tuple>
+
 using namespace std;
 
-static vector<int> parent;
-static vector<int> trueP;
-static vector<vector<int>> party;
-static int result;
+vector<int> v[32001];
+int inDegree[32001];
 
-void unionFunc(int a, int b);
-int find(int a);
-bool checkSame(int a, int b);
+int N, M;
+
+void TopologySort();
 
 int main()
-{ 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL); 
-
-	int N, M, T;
-	cin >> N >> M >> T;
-	trueP.resize(T);
-	party.resize(M);
-	parent.resize(N + 1);
-	  
-	for (int i = 0; i <= N; i++)
-		parent[i] = i;
-
-	for (int i = 0; i < T; i++)
-		cin >> trueP[i];
-
+{
+	cin >> N >> M;
+	int a, b;
 	for (int i = 0; i < M; i++)
 	{
-		int party_size;
-		cin >> party_size;
+		cin >> a >> b;
+		v[a].push_back(b);
+		inDegree[b]++;
+	}
+	TopologySort();
+	return 0;
+}
 
-		for (int j = 0; j < party_size; j++)
+void TopologySort()
+{
+	queue<int> q; 
+	for (int i = 1; i <= N; i++) 
+	{
+		if (inDegree[i] == 0)
+			q.push(i);
+	} 
+
+	while (q.size())
+	{
+		int n = q.front();
+		q.pop(); 
+
+		cout << n << " ";
+
+		for (int i = 0; i < v[n].size(); i++)  
 		{
-			int temp;
-			cin >> temp;
-			party[i].push_back(temp);
+			int id = v[n][i];
+			if (--inDegree[id] == 0)
+				q.push(id) ;
 		}
 	}
 
-	for (int i = 0; i < M; i++)
-	{
-		int firstPeople = party[i][0];
-		for (int j = 1; j < party[i].size(); j++)
-			unionFunc(firstPeople, party[i][j]); 
-	}
-
-	for (int i = 0; i < M; i++)
-	{
-		bool isPossible = true;
-		int cur = party[i][0];
-		for (int j = 0; j < T; j++)
-		{
-			if (find(cur) == find(trueP[j]))
-			{
-				isPossible = false;
-				break;
-			}
-		}
-		if (isPossible)
-			result++;
-	}
-	cout << result << "\n";
-	
-}   
-
-void unionFunc(int a, int b)
-{
-	int A = find(a);
-	int B = find(b);
-	
-	if (A > B)
-		parent[A] = B;
-
-	else
-		parent[B] = A;
-} 
- 
-int find(int a) 
-{
-	if (parent[a] != a)
-		parent[a] = find(parent[a]); // 경로 압축
-	return parent[a];
 }
-
-bool checkSame(int a, int b)
-{
-	int A = find(a);
-	int B = find(b); 
-
-	return A == B; 
-}
-
