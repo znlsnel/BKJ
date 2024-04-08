cca89157e79637fa9a9f3ea8f5d38145e1e46719
Udi <dnel0605@gmail.com>
2024-04-08 22:41:35 +0900

    1854 K번째 최단경로 찾기 (다익스트라) [Platinum IV]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -6,61 +6,62 @@
 using namespace std;
 typedef pair<int, int> edge;
 
-vector<vector<edge>> edges;
-priority_queue<edge, vector<edge>, greater<edge>> q;
+int N, M, K;
+static int W[1001][1001];
 vector<int> mdistance;
-vector<int> parent;
-vector<bool> visited;
+priority_queue<int> distQueue[1001];
 
 int main()
 {
-	int V, E;
-	cin >> V >> E;
-	edges.resize(V + 1);
-	parent.resize(V + 1);
-	visited.resize(V + 1, false);
-	mdistance.resize(V + 1, 100000000);
 
-	for (int i = 0; i < E; i++) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < M; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
 
-		edges[u].push_back(make_pair(v, w));
-	}
+		W[u][v] = w; 
+	} 
 
-	int S, D;
-	cin >> S >> D;
-	q.push(make_pair(0, S));
-	mdistance[S] = 0;
 
-	while (q.size())
+	priority_queue<edge, vector<edge>, greater<edge>> pq;
+	pq.push(make_pair(0, 1));
+	distQueue[1].push(0);
+
+	while (pq.size())
 	{
-		edge current = q.top();
-		q.pop();
-		int curNode = current.second;
+		edge current = pq.top();
+		pq.pop();
 
-		if (visited[curNode])
-			continue;
-
-		visited[curNode] = true;
-
-		for (edge e : edges[curNode]) {
-			int nextDist = e.second + mdistance[curNode];
-			if (nextDist < mdistance[e.first]) {
-				mdistance[e.first] = nextDist;
-				q.push(make_pair(mdistance[e.first], e.first));
+		for (int adjNode = 1; adjNode <= N; adjNode++)
+		{
+			if (W[current.second][adjNode] != 0) 
+			{
+				if (distQueue[adjNode].size() < K)
+				{
+					distQueue[adjNode].push(current.first + W[current.second][adjNode]);
+					pq.push(make_pair(current.first + W[current.second][adjNode], adjNode));
+				}
+				else if (distQueue[adjNode].top() > current.first + W[current.second][adjNode])
+				{
+					distQueue[adjNode].pop();
+					distQueue[adjNode].push(current.first + W[current.second][adjNode]);
+					pq.push(make_pair(current.first + W[current.second][adjNode], adjNode));
+				}
 			}
 		}
 	}
-	cout << mdistance[D] << "\n";
+	for (int i = 1; i <= N; i++) {
+		if (distQueue[i].size() == K)
+			cout << distQueue[i].top() << "\n";
+		else
+			cout << -1 << "\n";
+	}
 
-	//for (int i = 1; i <= V; i++)
-	//{
-	//	if (visited[i])
-	//		cout << mdistance[i] << "\n";
-	//	else
-	//		cout << "INF" << "\n"; 
-	//}
 
 	return 0;
 }
