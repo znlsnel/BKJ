32ce91f6db099111a7ffb3e84bdf2d61c07ab2eb
Udi <dnel0605@gmail.com>
2024-04-15 11:45:37 +0900

    1197 최소 스패닝 트리 (최소 스패닝 트리) [Gold IV]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -10,43 +10,69 @@ static int N;
 static long mdistance[101][101];
 
 
+void Union(int a, int b);
+int find(int a);
+static vector<int> parent;
+
+typedef struct Edge {
+	int s, e, v;
+	bool operator > (const Edge& temp) const {
+		return v > temp.v;
+	}
+} Edge;
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
+	priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+	parent.resize(N + 1);
 
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-			cin >> mdistance[i][j];
+	for (int i = 0; i <= N; i ++ ) {
+		parent[i] = i;
 	}
 
-	for (int k = 0; k < N; k++)
+	for (int i = 0; i < M; i++)
 	{
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < N; j++)
-			{
-				if (mdistance[i][k] == 1 && mdistance[k][j] == 1)
-					mdistance[i][j] = 1;
-			}
+		int s, e, v;
+		cin >> s >> e >> v;
+		pq.push(Edge{ s, e, v });
+	}
+
+	int useEdge = 0;
+	int result = 0;
+
+	while (useEdge < N - 1)
+	{
+		Edge now = pq.top();
+		pq.pop();
+
+		if (find(now.s) != find(now.e)) {
+			Union(now.s, now.e);
+			result = result + now.v;
+			useEdge++; 
 		}
 	}
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			if (mdistance[i][j] == 1) 
-				cout << "1 "; 
-			else
-				cout << "0 ";
-		}
-		cout << "\n";
-	}
-
+	cout << result;
 }
 
+void Union(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+
+	if (a != b)
+		parent[b] = a;
+}
+
+int find(int a)
+{
+	if (a == parent[a])
+		return a;
+	 
+	return parent[a] = find(parent[a]);
+}
