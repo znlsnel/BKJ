7d4fb4f4ad212557d3fb47f0eb126a95c722c409
Udi <dnel0605@gmail.com>
2024-04-17 14:25:47 +0900

    1414 불우이웃돕기 (최소 스패닝 트리) [Gold III]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -7,18 +7,10 @@
 using namespace std;
 
 
-static int dr[] = { -1, 0, 1, 0 };
-static int dc[] = { 0, 1, 0, -1 };
-static int map[201][201]; 
-static bool visited[201][201] = { false }; 
 static vector<int> parent; 
-static int N, M , sNum;
-static vector<vector<pair<int, int>>> sumList;
-static vector<pair<int, int>> mList;
 
 void Union(int a, int b);
 int find(int a); 
-void BFS(int i, int j);
 
 typedef struct Edge {
 	int s, e, v;
@@ -36,100 +28,53 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> M;
-	
+	int N, sum = 0;
+	cin >> N;
+	parent.resize(N);
 	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
-
-	sNum = 1;
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (map[i][j] != 0 && visited[i][j] != true)
-			{
-				BFS(i, j);
-				sNum++;
-				sumList.push_back(mList);
-			}
-		}
-	}
-
-	for (int i = 0; i < sumList.size(); i++)
-	{
-		vector<pair<int, int>> now = sumList[i];
-
-		for (int j = 0; j < now.size(); j++)
-		{
-			int r = now[j].first;
-			int c = now[j].second;
-			int now_S = map[r][c];
-
-			for (int d = 0; d < 4; d++)
-			{
-				int tempR = dr[d];
-				int tempC = dc[d];
-				int blenght = 0;
-
-				while (r + tempR >= 0 && r + tempR < N && c + tempC >= 0 && c + tempC < M)
-				{
-					if (map[r + tempR][c + tempC] == now_S)
-						break;
-
-					else if (map[r + tempR][c + tempC] != 0)
-					{
-						if (blenght > 1)
-							pq.push(Edge{ now_S, map[r + tempR][c + tempC], blenght });
-						break;
-					}
-					else
-						blenght++;
-
-					if (tempR < 0)
-						tempR--;
-					else if (tempR > 0)
-						tempR++;
-					else if (tempC < 0)
-						tempC--;
-					else if (tempC > 0)
-						tempC++;
-				} 
-			} 
-		}
-	}
-	parent.resize(sNum); 
-
-	for (int i = 0; i < parent.size(); i++)
-	{
 		parent[i] = i;
-	}
+	priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			char tempc = cin.get();
+			if (tempc == '\n')
+				tempc = cin.get();
+
+			int cost = 0;
+			if (tempc >= 'a' && tempc <= 'z')
+				cost = tempc -  'a' + 1;
+
+			else if (tempc >= 'A' && tempc <= 'Z')
+				cost = tempc - 'A' + 27;
+
+			sum += cost;
+			if (i != j && cost > 0)
+				pq.push(Edge{ i, j, cost });
+		}
+
 
 	int useEdge = 0;
 	int result = 0;
-
-	while (!pq.empty())
+	while (pq.size())
 	{
 		Edge now = pq.top();
-		pq.pop(); 
+		pq.pop();
 
-		if (find(now.s) != find(now.e)) {
+		if (find(now.s) != find(now.e))
+		{
 			Union(now.s, now.e);
-			result = result + now.v;
+			result += now.v;
 			useEdge++;
 		}
-
 	}
 
-	if (useEdge == sNum - 2)
-		cout << result << "\n";
+	if (useEdge == N - 1)
+		cout << sum - result << "\n";
 	else
-		cout << "-1" << "\n"; 
+		cout << -1 << "\n";
+
 }
 
 void Union(int a, int b)
@@ -149,56 +94,3 @@ int find(int a)
 	return parent[a] = find(parent[a]);
 }
 
-void BFS(int i, int j)
-{
-	queue<pair<int, int>> myQueue;
-	mList.clear();
-	myQueue.push(make_pair(i, j));
-	mList.push_back(make_pair(i, j));
-	visited[i][j] = true;
-	map[i][j] = sNum;
-
-	while (!myQueue.empty())
-	{
-		int r = myQueue.front().first;
-		int c = myQueue.front().second;
-		myQueue.pop();
-
-		for (int d = 0; d < 4; d++)
-		{
-			int tempR = dr[d];
-			int tempC = dc[d];
-
-			auto isInBox = [&]() { return r + tempR >= 0 && r + tempR < N && c + tempC >= 0 && c + tempC < M; };
-			while (isInBox())
-			{
-				bool isVisited = visited[r + tempR][c + tempC];
-				bool island = map[r + tempR][c + tempC] != 0;
-				if (isVisited == false && island)
-				{
-					int now_i = r + tempR;
-					int now_j = c + tempC;
-					map[now_i][now_j] = sNum;
-					visited[now_i][now_j] = true;
-					mList.push_back(make_pair(now_i, now_j));
-					myQueue.push(make_pair(now_i, now_j));
-				}
-				else
-					break;
-				
-				if (tempR < 0)
-					tempR--;
-				else if (tempR > 0)
-					tempR++;
-				else if (tempC < 0)
-					tempC--;
-				else if (tempC > 0)
-					tempC++;
-			}
-
-
-		}
-
-	}
-
-}
