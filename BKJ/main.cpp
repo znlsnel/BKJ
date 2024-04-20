bfb85c865b51094dd40d8d592618942d180d5498
Udi <dnel0605@gmail.com>
2024-04-20 12:14:04 +0900

    14425 문자열 집합 (Trie) [Silver IV]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -7,51 +7,78 @@
 using namespace std;
 
 
-void dfs(int node);
-static vector<vector<int>> tree;
-static int M;
-int result = 0;
+static int N, M;
+
+class Node
+{
+public:
+	Node* childs[26];
+	bool isLeaf;
+	Node() : isLeaf(false)
+	{
+		fill(childs, childs + 26, nullptr);
+	}
+	~Node() {
+		for (auto& child : childs)
+			delete child;
+	}
+
+	void insert(const char* key)
+	{
+		if (*key == 0)
+			isLeaf = true;
+		else
+		{
+			int next_index = *key - 'a';
+			if (childs[next_index] == nullptr)
+				childs[next_index] = new Node();
+
+			childs[next_index]->insert(key + 1);
+		}
+
+	}
+
+	Node* find(const char* key)
+	{
+		if (*key == 0)
+			return this;
+
+		int next_index = *key - 'a';
+
+		if (childs[next_index] == nullptr)
+			return nullptr;
+		return childs[next_index]->find(key + 1);
+	}
+};
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N;
-	cin >> N;
-	tree.resize(N + 1);
-	int root = 0;
+	cin >> N >> M;
+	Node* root = new Node();
 
-	for (int i = 0; i < N; i++)
+	while (N--)
 	{
-		int node;
-		cin >> node;
-		if (node != -1 )
-			tree[node].push_back(i); 
-		else
-			root = i;
-
+		char text[501];
+		cin >> text;
+		root->insert(text);
 	}
-	cin >> M;
-	if (M != root)
-		dfs(root);
 
-	cout << result;
+	int count = 0;
+
+	while (M--)
+	{
+		char text[501];
+		cin >> text;
+		Node* result = root->find(text);
+
+		if (result && result->isLeaf)
+			count++;
+	}
+	cout << count << "\n";
+
 } 
  
-void dfs(int node)
-{
-	int count = 0;
-	for (int n : tree[node])
-	{
-		if (n == M ) 
-			continue;
-
-		count++;
-		dfs(n);
-
-	}
-	if (count == 0) 
-		result++;
-}
-
