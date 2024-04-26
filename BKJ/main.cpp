f4e72108a97cc7ffed2cf45018d85a56568e6f0a
Udi <dnel0605@gmail.com>
2024-04-26 10:58:28 +0900

    11438 LCA2 (최소 공통 조상) [Platinum V]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -9,11 +9,12 @@ using namespace std;
 
 vector<vector<int>> tree;
 vector<int> depth;
-vector<int> parent;
 vector<bool> visited;
-int executeLCA(int a, int b);
-void BFS(int i);
+static int parent[21][100001];
+static int kmax;
 
+void BFS(int i);
+int LCA(int a, int b);
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -22,12 +23,9 @@ int main()
 
 	int N;
 	cin >> N; 
-
 	tree.resize(N + 1);
 	depth.resize(N + 1);
-	parent.resize(N + 1);
 	visited.resize(N + 1);
-
 
 	for (int i = 0; i < N - 1; i++)
 	{
@@ -35,68 +33,89 @@ int main()
 		cin >> a >> b;
 		tree[a].push_back(b);
 		tree[b].push_back(a);
+	 }
+
+	int temp = 1;
+	kmax = 0;
+	while (temp <= N) {
+		temp <<= 1;
+		kmax++;
 	}
-	parent[1] = 0; 
+
 	BFS(1);
-	 
+
+	for (int k = 1; k <= kmax; k++) {
+		for (int n = 1; n <= N; n++) {
+			parent[k][n] = parent[k - 1][parent[k - 1][n]];
+		}
+	}
+
+	
+	vector<int> result;
 	int M;
 	cin >> M;
-
-	vector<int> result;
 	for (int i = 0; i < M; i++)
 	{
 		int a, b;
 		cin >> a >> b;
-		result.push_back(executeLCA(a, b));
+		result.push_back(LCA(a, b));
 	}
-
 	for (int i : result)
 		cout << i << "\n";
-	 
-}
 
-int executeLCA(int a, int b)
-{
-	if (depth[a] < depth[b])
-	{
+}
+int LCA(int a, int b) {
+	if (depth[a] > depth[b]) {
 		int temp = a;
 		a = b;
 		b = temp;
 	}
-	while (depth[a] != depth[b])
-		a = parent[a];
-	
-	while (a != b) {
-		a = parent[a];
-		b = parent[b];
+
+	for (int k = kmax; k >= 0; k--)
+	{
+		if (pow(2, k) <= depth[b] - depth[a]) {
+			if (depth[a] <= depth[parent[k][b]])
+				b = parent[k][b];
+		}
 	}
-	return a;
+	for (int k = kmax; k >= 0 && a != b; k--) {
+		if (parent[k][a] != parent[k][b]) {
+			a = parent[k][a];
+			b = parent[k][b];
+		}
+	}
+	int LCA = a;
+	if (a != b)
+		LCA = parent[0][LCA];
+
+	return LCA;
 }
-  
+
+ 
+
 void BFS(int i)
-{ 
-	// index, level
+{
 	queue<pair<int, int>> q;
 	q.push(make_pair(i, 0));
 
-	 
 	while (q.size())
 	{
-		int index = q.front().first;
-		int level = q.front().second;
-		
-		visited[index] = true;
-		depth[index] = level;
+		int curId = q.front().first;
+		int curLevel = q.front().second;
 		q.pop();
 
-		 
-		for (int i : tree[index]) {
-			
-			if (visited[i] == true) 
+		visited[curId] = true;
+		depth[curId] = curLevel;
+
+		for (int i : tree[curId]) {
+			if (visited[i])
 				continue;
 
-			parent[i] = index;
-			q.push(make_pair(i, level + 1)); 
+			parent[0][i] = curId; 
+
+
+			q.push(make_pair(i, curLevel + 1));
 		}
 	}
 }
+
