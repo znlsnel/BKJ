85b0e3ce9623f20c993f8054aceb8bd8666e4be1
Udi <dnel0605@gmail.com>
2024-04-27 15:52:04 +0900

    11051 이항 계수2 (조합론) [Silver II]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 static int N, K;
-static int D[11][11];
+static int D[1001][1001]; 
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -26,8 +26,14 @@ int main()
 	for (int i = 2; i <= N; i++) {
 		for (int j = 1; j < i; j++) {
 			D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+			D[i][j] = D[i][j] % 10007;
 		}
 	}
 
 	cout << D[N][K] << "\n";
 }
+
+// 15 / 6 * (5 - 3)^2
+// 15 / 6 * 4
+// 15 / 24
+// 5/ 8
