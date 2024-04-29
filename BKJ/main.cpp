509cc9fbc75e4d724c29f0a8c2855b4027638d26
Udi <dnel0605@gmail.com>
2024-04-29 10:29:48 +0900

    1010 다리 놓기 (조합론) [Silver V]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -7,30 +7,36 @@
 
 using namespace std;
 
-static int N, K;
 static int D[1001][1001]; 
+
+int array[5][5] = {
+	{1, 0, 0, 0, 0}, // 0개 0 개 선택
+	{1, 1, 0, 0, 0}, // 1개 0, 1개 선택
+	{1, 2, 1, 0, 0}, // 2개 0, 1, 2개 선택
+	{1, 3, 0, 1, 0}, // 3개 0, 1, 3개 선택
+	{1, 4, 0, 0, 1}, // 4개 0, 1, 4개 선택
+};
 
 void result()
 {
-	int k, n;
-	cin >> k >> n;
+	int N, M;
+	cin >> N >> M;
+	 
+	for (int i = 0; i <= M; i++) {
+		D[i][1] = i; 
+		D[i][0] = 1;
+		D[i][i] = 1;  
+	}
 
-	for (int i = 1; i <= n; i++) {
-		D[0][i] = i; 
-	}
-	for (int i = 1; i <= k; i++) {
-		D[i][1] = 1;
-	}
-	
-	for (int i = 1; i <= k; i++) 
+	for (int i = 3; i <= M; i++) 
 	{
-		for (int j = 2; j <= n; j++) 
+		for (int j = 2; j <= N; j++) 
 		{
-			D[i][j] = D[i][j - 1] + D[i - 1][j];
+			D[i][j] = D[i - 1][j - 1] + D[i - 1][j]; 
 		}
 	}
-
-	cout << D[k][n] << "\n";
+	 
+	cout << D[M][N] << "\n";  
 }
 int main()
 {
