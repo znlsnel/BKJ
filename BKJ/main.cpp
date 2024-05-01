c12c421df1ce75725f4d901ef8097369d1bca18e
Udi <dnel0605@gmail.com>
2024-05-01 11:40:43 +0900

    1256 사전 (조합론) [Gold II]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -7,74 +7,72 @@
 
 using namespace std;
 
-static int N, Q;
-static long F[21], S[21];
-static bool visited[21] = { false };
-
-void answer1();
-void answer2();
-
+static int N, M, K;
+static long C[202][202];
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); 
 	cout.tie(NULL);
 
-	cin >> N >> Q;
-
-	F[0] = 1;
-	for (int i = 1; i < N; i++) {
-		F[i] = F[i - 1] * i;
-	}
-
-	if (Q == 1) {
-		answer1();
-	}
-	else if (Q == 2) {
-		answer2();
-	}
-}
-void answer1() 
-{
-	long K;
-	cin >> K; 
-
-	for (int i = 1; i <= N; i++) 
-	{
-		for (int j = 1, cnt = 1; i <= N; j++)
-		{
-			if (visited[j])
-				continue;
-
-			if (K <= F[N - i] * cnt) {
-				K -= F[N - i] * (cnt - 1);
-				S[i] = j;
-				visited[j] = true;
-				break;
-			}
-			cnt++;
+	// N개의 a, M개의 z
+	cin >> N >> M >> K;
+	
+	for (int i = 0; i <= 200; i++) {
+		C[i][i] = 1;
+		C[i][0] = 1; 
+		C[i][1] = i; 
+	} 
+	for (int i = 3; i <= 200; i++) { 
+		for (int j = 2; j <= i; j++) { 
+			C[i][j] = C[i - 1][j - 1] + C[i - 1][j];  
+			if (C[i][j] > 1000000000)
+				C[i][j] = 1000000001;
 		}
 	}
 
-	for (int i = 1; i <= N; i++)
-		cout << S[i] << " ";
-}
-void answer2()
-{
-	long K = 1;
-	for (int i = 1; i <= N; i++)
-	{
-		cin >> S[i];
-		int cnt = 0;
-		for (int j = 1; j < S[i]; j++)
-		{
-			if (visited[j] == false)
-				cnt++;
-		}
-		K += F[N - i] * cnt; 
-		visited[S[i]] = true;
+
+
+	// 100 * 100 
+	// 1, 2, 3, 4, 5
+	// 120, 24, 6, 2, 1
+	 // N = 2, M = 2, K = 6라고 할때
+	 //
+	 // 24 6 2 1
+	// N이 앞에 올 경우의 수 = 3 
+	// M이 앞에 올 경우의 수 = 3
+	// N N M M
+	// N M N M  
+	// N M M N   
+	// M M N N  
+	// M N M M
+	// M N N M
+
+	if (C[N + M][M] < K) {
+		cout << "-1";
+		return 0;
 	}
 
-	cout << K << "\n";
+
+	vector<char> result;
+	int k = K;
+	while (N + M) { 
+		// n이 앞 나올 경우의 수 
+		long n = C[N - 1 + M][M];   
+
+		if (k <= n) {  
+			result.push_back('a');
+			N--;
+		}  
+		else {
+			result.push_back('z');
+			M--; 
+			k -= n;
+		}  
+	}
+
+	for (char r : result)
+		cout << r; 
+
 }
 
