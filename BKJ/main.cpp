d6c9f4d9b896f2acd833bc0349d18573410eead3
Udi <dnel0605@gmail.com>
2024-05-07 22:52:29 +0900

    13398 연속합2 (동적계획법) [Gold V]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -6,9 +6,9 @@
 #include <cmath>
 
 using namespace std;
-
+ 
 static int N;
-static long long D[101][10];
+static vector<int> A, L, R;
 
 // 1일 때 = 9
 // 2의 경우의 수 = 1의 경우의 수 * 2 - 1
@@ -19,25 +19,33 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	 
 	cin >> N;
+	A.resize(N);
 
-	for (int i = 1; i < 10; i++)
-		D[1][i] = 1;
-
-	for (int i = 2; i <= N; i++) {
-		D[i][0] = D[i - 1][1];
-		D[i][9] = D[i - 1][8];
-
-		for (int j = 1; j <= 8; j++) {
-			D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % 1000000000;
-		}
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
 	}
 
-	long sum = 0;
-	for (int i = 0; i < 10; i++) {
-		sum = (sum + D[N][i]) % 1000000000;
+	L.resize(N);
+	L[0] = A[0];
+	int result = L[0];
+
+	for (int i = 1; i < N; i++) {
+		L[i] = max(A[i], L[i - 1] + A[i]);
+		result = max(result, L[i]);
+	}
+	
+	R.resize(N);
+	R[N - 1] = A[N - 1];
+	for (int i = N - 2; i >= 0; i--) {
+		R[i] = max(A[i], R[i + 1] + A[i]);
 	}
 
-	cout << sum;
+	for (int i = 1; i < N - 1; i++) {
+		int temp = L[i - 1] + R[i + 1];
+		result = max(result, temp);
+	}
+
+	cout << result;
 }
