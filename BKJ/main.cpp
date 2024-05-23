53d02a45503523d9dcd07229160ff01fc4af5a55
Udi <dnel0605@gmail.com>
2024-05-23 14:18:32 +0900

    PGS_ 산 모양 타일링 (동적 계획법) [Lv.3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,84 +1,21 @@
 ﻿#include <string>
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
+int solution(int n, vector<int> tops) {
+        vector<int> v(n + 1);
+        v[0] = 1;
+        v[1] = v[0] * (3 + tops[0]);
 
-static int combine, StartNum, targetValue, specialCase;
-
-int solution(int coin, vector<int> cards)
-{
-        int answer = 1;
-        targetValue = cards.size() + 1;
-        StartNum = cards.size() / 3;
-
-        for (int i = 1; i < StartNum; i++)
-        {
-                for (int j = 0; j < i; j++)
-                {
-                        if (cards[i] + cards[j] == targetValue)
-                                combine++;
-                }
+        for (int i = 2; i <= n; i++) {
+                v[i] = (v[i - 1] * (3 + tops[i - 1]) - v[i - 2]) % 10007;
+                if (v[i] < 0) v[i] += 10007;  // 음수를 방지
         }
 
-        for (int i = StartNum; i < cards.size(); i += 2)
-        {
-                for (int j = 0; j < i; j++)
-                {
-                        if (j < StartNum)
-                        {
-                                if (coin > 0 && cards[i] + cards[j] == targetValue)
-                                {
-                                        combine++;
-                                        coin--;
-                                }
-
-                                if (coin > 0 && cards[i + 1] + cards[j] == targetValue)
-                                {
-                                        combine++;
-                                        coin--;
-                                }
-                        }
-                        else
-                        {
-                                if (cards[i] + cards[j] == targetValue)
-                                        specialCase++;
-
-                                if (cards[i + 1] + cards[j] == targetValue)
-                                        specialCase++;
-                        }
-                }
-
-                if (cards[i] + cards[i + 1] == targetValue)
-                {
-                        specialCase++;
-                }
-
-                if (combine > 0)
-                {
-                        answer++;
-                        combine--;
-                }
-                else
-                {
-                        if (specialCase > 0 && coin >= 2)
-                        {
-                                coin -= 2;
-                                specialCase--;
-                                answer++;
-                        }
-                        else
-                        {
-                                break;
-                        }
-                }
-
-        }
-
-        return answer;
+        return v[n];
 }
-
 int main() 
 {
         ios::sync_with_stdio(false);
