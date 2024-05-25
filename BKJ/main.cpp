5e53f88176c563270bb644081d027bb35141350c
Udi <dnel0605@gmail.com>
2024-05-25 16:02:08 +0900

    PGS_ 주사위 고르기 (Lv.3) 90점
    100점은 못맞음.. 다시 도전

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,91 +1,223 @@
-﻿#include <iostream>
-#include <vector>
-#include <cmath>
+﻿#include <string>
+#include <iostream>
 #include <algorithm>
-
+#include <vector>
+#include <set>
 
 using namespace std;
 
-class UF
+vector<vector<int>> _diceSet;
+vector<vector<int>> _diceCombi;
+vector<vector<int>> diceCombis;
+
+
+struct Node
 {
-public:
-	static int FindParent(int parent[], int n)
-	{
-		if (parent[n] == n) return n;
-		return parent[n] = FindParent(parent, parent[n]);
-	}
+        vector<vector<int>>* _diceCombis = nullptr;
 
-	static void UnionParent(int parent[], int a, int b)
-	{
-		a = FindParent(parent, a);
-		b = FindParent(parent, b);
-		if (a < b) parent[b] = a;
-		else parent[a] = b;
-	}
+        int index = 0;
 
-	static bool GetParent(int parent[], int a, int b)
-	{
-		if (FindParent(parent, a) == FindParent(parent, b)) return false;
-		else return true;
-	}
+        Node* _left = nullptr;
+        Node* _right = nullptr;
+
+        Node(vector<vector<int>>* diceCombis, int id)
+        {
+                _diceCombis = diceCombis;
+                index = id;
+        }
+
+        void Insert(int id)
+        {
+                int a = 0, b = 0;
+                int winner = compare(index, id, a, b);
+
+                if (winner == index)
+                {
+                        if (_right == nullptr)
+                                _right = new Node(_diceCombis, id);
+
+                        else
+                                _right->Insert(id);
+                }
+                else
+                {
+                        if (_left == nullptr)
+                                _left = new Node(_diceCombis, id);
+
+                        else
+                                _left->Insert(id);
+                }
+
+        }
+
+        int compare(int a, int b, int& score_A, int& score_B)
+        {
+
+                vector<vector<int>>& diceCombis = *_diceCombis;
+
+                for (int cur : diceCombis[a])
+                {
+                        int start = 0;
+                        int end = diceCombis[b].size() - 1;
+                        while (start <= end)
+                        {
+                                int mid = (start + end) / 2;
+
+                                if (diceCombis[b][mid] >= cur) {
+                                        end = mid - 1;
+                                }
+
+                                else {
+                                        start = mid + 1;
+                                }
+                        }
+                        score_A += diceCombis[b][start] < cur ? start + 1 : start;
+                }
+
+                for (int cur : diceCombis[b])
+                {
+                        int start = 0;
+                        int end = diceCombis[a].size() - 1;
+                        while (start <= end)
+                        {
+                                int mid = (start + end) / 2;
+
+                                if (diceCombis[a][mid] >= cur) {
+                                        end = mid - 1;
+                                }
+
+                                else {
+                                        start = mid + 1;
+                                }
+                        }
+                        score_B += diceCombis[a][start] < cur ? start + 1 : start;
+                }
+
+                /*cout << "======= FIGHT ! ! ========\n";
+                cout << a << ": \n";
+                for (int i : diceCombis[a])
+                    cout << i << ", ";
+                cout << "\n";
+                cout << "score : " << score_A << "\n";
+
+                cout << b << ": \n";
+                for (int i : diceCombis[b])
+                    cout << i << ", ";
+                cout << "\n";
+                cout <<"score : " << score_B << "\n";
+
+                cout <<"\n\n";*/
+
+                //score = max(score_A, score_B);
+
+                return score_A >= score_B ? a : b;
+        }
+
+        void Inorder(vector<int>* v)
+        {
+                if (_left != nullptr)
+                        _left->Inorder(v);
+
+                if (v->size() == 0)
+                {
+                        v->push_back(index);
+                }
+        }
 };
 
-class Edge
+
+void MakeSet(vector<int>v, vector<bool> visited, int size)
 {
-public:
-	Edge(int a, int b, float d) {
-		Node[0] = a;
-		Node[1] = b;
-		distance = d;
-	}
-	bool operator < (Edge& edge)
-	{
-		return this->distance < edge.distance;
-	}
-public:
-	int Node[2];
-	float distance;
-};
-int main()
+        if (v.size() == size / 2)
+        {
+                _diceSet.push_back(v);
+                return;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+                if (visited[i])
+                        continue;
+
+                visited[i] = true;
+
+                vector<bool> newB = visited;
+                vector<int> newV = v;
+                newV.push_back(i);
+
+                MakeSet(newV, newB, size);
+        }
+}
+
+void MakeSet2(vector<int>v, int size, int start = 0)
 {
-	vector<Edge> v;
-	vector<pair<int, int>> pv;
-	int parent[101]{ 0 };
-	int N;
-	cin >> N;
+        if (v.size() == size / 2)
+        {
+                _diceCombi.push_back(v);
+                return;
+        }
 
-	for (int i = 0; i < N; i++)
-	{
-		float a, b;
-		parent[i] = i;
-		cin >> a >> b;
-		pv.push_back(make_pair(a, b));
-	}
+        for (int i = start; i < 6; i++)
+        {
+                vector<int> newV = v;
+                newV.push_back(i);
 
-	for (int i = 0; i < N - 1; i++)
-	{
-		for (int j = i + 1; j < N; j++)
-		{
-			float dist = static_cast<float>(
-				sqrt(pow(pv[j].first - pv[i].first, 2) +
-					pow(pv[j].second - pv[i].second, 2)));
-			v.push_back(Edge(i, j, dist));
-		}
-	}
-	sort(v.begin(), v.end());
-
-	float sum = 0;
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (UF::GetParent(parent, v[i].Node[0], v[i].Node[1]))
-		{
-			sum += v[i].distance;
-			UF::UnionParent(parent, v[i].Node[0], v[i].Node[1]);
-		}
-	}
+                MakeSet2(newV, size, start);
+        }
+}
 
 
-	cout << round(sum * 100) / 100;
+vector<int> solution(vector<vector<int>> dice) {
+        vector<int> answer;
 
-	return 0;
+        MakeSet(vector<int>(), vector<bool>(dice.size()), dice.size());
+        MakeSet2(vector<int>(), dice.size());
+
+        for (auto diceSet : _diceSet)
+        {
+                vector<int> numbers;
+                for (auto c : _diceCombi)
+                {
+                        int temp = 0;
+                        for (int i = 0; i < c.size(); i++)
+                        {
+                                int a = diceSet[i];
+                                temp += dice[a][c[i]];
+                        }
+                        numbers.push_back(temp);
+                }
+                sort(numbers.begin(), numbers.end());
+                diceCombis.push_back(numbers);
+        }
+
+
+        Node* node = new Node(&diceCombis, 0);
+        for (int i = 1; i < _diceSet.size(); i++)
+                node->Insert(i);
+
+        /* vector<int> v(_diceSet.size());
+         for (int i = 0; i < _diceSet.size(); i++){
+             for (int j = i + 1; j < _diceSet.size(); j++)
+             {
+                 int a = 0;
+                 int b = 0;
+                 node->compare(i, j, a, b);
+                 v[i] += a;
+                 v[j] += b;
+             }
+         }
+         int maxId = 0;
+         for (int i = 1; i < v.size(); i++)
+         {
+             if (v[maxId] < v[i])
+                 maxId = i;
+         }*/
+
+
+        node->Inorder(&answer);
+        answer = _diceSet[answer[0]];
+        for (int& i : answer)
+                ++i;
+
+        return answer;
 }
