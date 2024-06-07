4331a4e4f97b73ed9d09b3e9d17078a575ee4ef7
Udi <dnel0605@gmail.com>
2024-06-07 12:42:22 +0900

    등산코스 정하기 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,46 +1,82 @@
 ﻿#include <string>
 #include <vector>
-#define INF 10001
+#include <queue>
+#include <iostream>
+
 
 using namespace std;
 
-int solution(int alp, int cop, vector<vector<int>> problems) {
+vector<int> costs;
+vector<int> answer;
+vector<bool> tops;
 
-        int dp[151][151];
-        fill(&dp[0][0], &dp[150][150], INF);
+vector<vector<pair<int, int>>> v;
 
-        int goal_alp = 0;
-        int goal_cop = 0;
-        for (auto p : problems)
+void dijkstra(vector<int>& gates)
+{
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+        for (int gate : gates)
         {
-                goal_alp = max(p[0], goal_alp);
-                goal_cop = max(p[1], goal_cop);
+                q.push(make_pair(0, gate));
+                costs[gate] = 0;
         }
-        alp = min(alp, goal_alp);
-        cop = min(cop, goal_cop);
 
-        dp[alp][cop] = 0;
-        for (int i = alp; i <= goal_alp; i++)
+        while (!q.empty())
         {
-                for (int j = cop; j <= goal_cop; j++)
-                {
-                        if (i < goal_alp)
-                                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1);
-                        if (j < goal_cop)
-                                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1);
+                int node = q.top().second;
+                int cost = q.top().first;
+                q.pop();
 
-                        for (auto p : problems)
+                if (answer[0] > 0 && answer[1] < cost)
+                        continue;
+
+                if (tops[node])
+                {
+                        // cout << node << "\n";
+                        if (answer[0] == -1 || answer[1] > cost)
                         {
-                                if (i >= p[0] && j >= p[1])
-                                {
-                                        int nextAlp = min(i + p[2], goal_alp);
-                                        int nextCop = min(j + p[3], goal_cop);
-                                        dp[nextAlp][nextCop] = min(dp[nextAlp][nextCop], dp[i][j] + p[4]);
-                                }
+                                answer[0] = node;
+                                answer[1] = cost;
+                        }
+                        else if (answer[1] == cost && node < answer[0])
+                                answer[0] = node;
+
+                        continue;
+                }
+                //     cout << node << "\n";
+                    // cout << v[node].size() << "\n";
+                for (pair<int, int> next : v[node])
+                {
+                        int nextCost = max(next.second, cost);
+                        if (costs[next.first] == -1 || nextCost < costs[next.first])
+                        {
+                                q.push(make_pair(nextCost, next.first));
+                                costs[next.first] = nextCost;
                         }
                 }
         }
 
+}
 
-        return dp[goal_alp][goal_cop];
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
+
+        v.resize(n + 1);
+        tops.resize(n + 1, false);
+        answer.resize(2, -1);
+        costs.resize(n + 1, -1);
+
+        for (int t : summits) {
+                tops[t] = true;
+        }
+
+        for (int i = 0; i < paths.size(); i++)
+        {
+                v[paths[i][0]].push_back(make_pair(paths[i][1], paths[i][2]));
+                v[paths[i][1]].push_back(make_pair(paths[i][0], paths[i][2]));
+        }
+        dijkstra(gates);
+
+
+        return answer;
 }
