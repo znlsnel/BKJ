f29396c8b48566ce67be0d0a12ae69444659153e
Udi <dnel0605@gmail.com>
2024-06-12 11:48:36 +0900

    양궁대회 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,57 +1,57 @@
 ﻿#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<vector<int>> v;
+int MaxScore;
+vector<int> mine(11);
+vector<int> answer{ -1 };
 
-int maxSheep;
-void DFS(vector<int>& info, int node, int sheep, int wolf)
+void DFS(vector<int>& info, int idx, int coin, int score, int op_score)
 {
-        if (sheep > 0 && sheep == wolf)
-                return;
-
-        if (info[node] == 0)
-                sheep++;
-        else
-                wolf++;
-
-        if (maxSheep < sheep)
-                maxSheep = sheep;
-
-        for (int i = 0; i < v[node].size(); i++)
+        // 이기던가 지던가 (비기면 안됨)
+        if (idx == 11)
         {
-                vector<int> addNodes;
-                for (int j = 0; j < v[node].size(); j++)
+                mine[10] += coin;
+                if (score > op_score && MaxScore < (score - op_score))
                 {
-                        if (i == j)
-                                continue;
-
-                        addNodes.push_back(v[node][j]);
+                        answer = mine;
+                        MaxScore = score - op_score;
                 }
-
-                int cnt = addNodes.size();
-                int next = v[node][i];
-
-                for (int i = 0; i < cnt; i++)
+                else if (score > op_score && MaxScore == (score - op_score))
                 {
-                        v[next].push_back(addNodes[i]);
+                        for (int i = 10; i >= 0; --i)
+                        {
+                                if (mine[i] > answer[i])
+                                {
+                                        answer = mine;
+                                        break;
+                                }
+                                else if (mine[i] < answer[i])
+                                        break;
+                        }
                 }
-
-                DFS(info, next, sheep, wolf);
-
-                for (int i = 0; i < cnt; i++)
-                        v[next].pop_back();
+                return;
         }
+
+        // 이길 경우
+        if (info[idx] < coin)
+        {
+                mine[idx] = info[idx] + 1;
+                DFS(info, idx + 1, coin - mine[idx], score + (10 - idx), op_score);
+
+        }
+
+        // 질 경우
+        if (info[idx] > 0)
+                op_score = op_score + (10 - idx);
+
+        mine[idx] = 0;
+        DFS(info, idx + 1, coin, score, op_score);
 }
 
-int solution(vector<int> info, vector<vector<int>> edges) {
-        v.resize(info.size());
-
-        for (int i = 0; i < edges.size(); i++)
-                v[edges[i][0]].push_back(edges[i][1]);
-
-        DFS(info, 0, 0, 0);
-
-        return maxSheep;
+vector<int> solution(int n, vector<int> info) {
+        DFS(info, 0, n, 0, 0);
+        return answer;
 }
