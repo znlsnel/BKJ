5b874978b8bd907f8b06fff4e1df6fe43188d5eb
Udi <dnel0605@gmail.com>
2024-06-17 11:57:27 +0900

    베스트앨범 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,52 +1,149 @@
 ﻿#include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
 #include <iostream>
-#include <limits>
 
 using namespace std;
-const int INF = 200000000;
 
-vector<vector<int>> dp;
-
-
-int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-
-        dp.resize(n + 1, vector<int>(n + 1, INF));
-
-        for (auto fare : fares)
+int stoh(string str)
+{
+        int ret = 0;
+        for (char c : str)
         {
-                int A = fare[0];
-                int B = fare[1];
-                int cost = fare[2];
-
-                dp[A][B] = cost;
-                dp[B][A] = cost;
+                ret += c - 'a';
         }
+        return ret;
+}
 
-        for (int i = 1; i <= n; i++)
-                dp[i][i] = 0;
+struct Genre
+{
+        string name;
 
-        for (int k = 1; k <= n; k++)
+        int total;
+
+        int first;
+        int second;
+
+        int first_id;
+        int second_id;
+
+        void AddPlayTime(int t, int idx)
         {
-                for (int i = 1; i <= n; i++)
+                total += t;
+                if (first < t)
                 {
-                        for (int j = 1; j <= n; j++)
+                        second = first;
+                        second_id = first_id;
+
+                        first = t;
+                        first_id = idx;
+
+                }
+                else if (second < t)
+                {
+                        second = t;
+                        second_id = idx;
+                }
+
+        }
+};
+
+struct Node
+{
+        Node* left = nullptr;
+        Node* right = nullptr;
+
+        int strId = -1;
+        vector<Genre> genres;
+
+        void Add(string str, int strId, int value, int id)
+        {
+                if (this->strId == strId)
+                {
+                        int genreID = -1;
+                        for (int i = 0; i < genres.size(); i++)
                         {
-                                if (dp[i][k] < INF && dp[k][j] < INF) {
-                                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+                                if (genres[i].name == str)
+                                {
+                                        genreID = i;
+                                        break;
                                 }
                         }
+                        if (genreID == -1)
+                        {
+                                Genre g{ str, 0, 0, -1, -1 };
+                                g.AddPlayTime(value, id);
+
+                                genres.push_back(g);
+                        }
+                        else
+                        {
+                                genres[genreID].AddPlayTime(value, id);
+                        }
+                }
+                else if (this->strId > strId)
+                {
+                        // 왼쪽
+                        if (left == nullptr)
+                        {
+                                left = new Node();
+                                left->strId = strId;
+                        }
+                        left->Add(str, strId, value, id);
+
+                }
+                else if (this->strId < strId)
+                {
+                        // 오른쪽
+                        if (right == nullptr)
+                        {
+                                right = new Node();
+                                right->strId = strId;
+                        }
+                        right->Add(str, strId, value, id);
                 }
         }
 
+        void GetGenres(vector<Genre>& v)
+        {
+                if (left != nullptr)
+                        left->GetGenres(v);
 
-        int answer = INF;
-        for (int i = 1; i <= n; ++i) {
-                answer = min(answer, dp[s][i] + dp[i][a] + dp[i][b]);
+                for (auto g : genres)
+                        v.push_back(g);
 
+                if (right != nullptr)
+                        right->GetGenres(v);
+        }
+};
+
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+        vector<int> answer;
+
+        Node root;
+        for (int i = 0; i < genres.size(); i++)
+        {
+                int hash = stoh(genres[i]);
+                root.Add(genres[i], hash, plays[i], i);
         }
 
+        vector<Genre> v;
+        root.GetGenres(v);
 
+        auto compare = [](Genre a, Genre b) {
+                return a.total > b.total;
+                };
+
+        sort(v.begin(), v.end(), compare);
+
+        for (Genre& g : v)
+        {
+                answer.push_back(g.first_id);
+                if (g.second_id != -1)
+                        answer.push_back(g.second_id);
+        }
 
         return answer;
 }
