46348d2eee0bd2255e1e5cdff291da68e8208a73
Udi <dnel0605@gmail.com>
2024-06-20 09:11:12 +0900

    단어 변환 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,165 +1,141 @@
 ﻿#include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-struct priority_queue
+struct map
 {
-        //   0
-        //  1 2 
-        // 3 4
-        // 아래에서 올라가는 방식
-        void push(int value)
+        map(int size = 50)
         {
-                v.push_back(value);
+                table.resize(size);
+        }
+        int hash(string str, int size = 50)
+        {
+                int key = 0;
 
-                int i = v.size() - 1;
+                for (char& c : str)
+                        key += c - 'a';
 
-                while (i > 0)
-                {
-                        // 부모노드 -> -1 / 2
-                        int parent = (i - 1) / 2;
-                        if (v[parent] <= v[i])
-                                break;
-
-                        int t = v[i];
-                        v[i] = v[parent];
-                        v[parent] = t;
-
-                        i = parent;
-                }
+                key %= size;
+                return key;
         }
 
-        int top()
+        void insert(string key, int value)
         {
-                if (v.empty())
-                        return -1;
-
-                return v[0];
+                table[hash(key)].push_back(make_pair(key, value));
         }
 
-        // 가장 뒤의 값을 앞으로 옮기고 pop_back -> 내려가면서 정렬
-        void pop()
+        int get(string key)
         {
-                v[0] = v.back();
-                v.pop_back();
+                int ret = -1;
+                int hkey = hash(key);
 
-                int i = 0;
-
-                while (i < v.size() - 1)
+                for (auto p : table[hkey])
                 {
-                        int left = i * 2 + 1;
-                        int right = left + 1;
-
-                        if (left >= v.size())
-                                break;
-
-                        if (right >= v.size())
-                                right = left;
-
-                        if (v[left] > v[right])
-                                left = right;
-
-                        if (v[i] > v[left])
+                        if (p.first == key)
                         {
-                                int t = v[i];
-                                v[i] = v[left];
-                                v[left] = t;
+                                ret = p.second;
+                                break;
                         }
-
-                        i = left;
                 }
+
+                return ret;
         }
 
-        bool empty()
-        {
-                return v.empty();
-        }
-        vector<int> v;
+        vector<vector<pair<string, int>>> table;
+
 };
 
-void heapSort(vector<int>& c)
+struct Node
 {
-        int answer = 0;
+        vector<Node*> childs;
+        string value;
 
-        priority_queue pq;
-
-        for (int n : c)
-                pq.push(n);
-
-        for (int i = 0; i < c.size(); i++)
+        void Push(Node& str)
         {
-                c[i] = pq.top();
-                pq.pop();
-        }
-}
-
-vector<int> merge(vector<int>& a, vector<int>& b)
-{
-        vector<int> result;
-
-        int id_a = 0;
-        int id_b = 0;
-        while (id_a < a.size() && id_b < b.size())
-        {
-                int next = -1;
-
-                if (a[id_a] > b[id_b])
-                        next = b[id_b++];
-                else
-                        next = a[id_a++];
-
-                result.push_back(next);
-        }
-
-        while (id_a < a.size())
-                result.push_back(a[id_a++]);
-
-        while (id_b < b.size())
-                result.push_back(b[id_b++]);
-
-        return result;
-}
-void mergeSort(vector<int>& c)
-{
-        if (c.size() <= 1)
-                return;
-
-        size_t mid = c.size() / 2;
-
-        vector<int> a(c.begin(), c.begin() + mid);
-        vector<int> b(c.begin() + mid, c.end());
-
-        mergeSort(a);
-        mergeSort(b);
-
-        c = merge(a, b);
-}
-
-int solution(vector<int> c) {
-        int answer = 0;
-
-        //  heapSort(c);
-        mergeSort(c);
-        //sort(c.begin(), c.end());
-
-        for (int i = 0; i < c.size(); i++)
-        {
-                int n = c.size() - i;
-                if (c[i] >= n)
+                int cnt = 0;
+                for (int i = 0; i < str.value.size(); i++)
                 {
-                        answer = n;
-                        break;
+                        if (str.value[i] != value[i])
+                                cnt++;
+
+                        if (cnt > 1)
+                                return;
+                }
+
+                if (cnt == 1)
+                        childs.push_back(&str);
+        }
+};
+vector<Node> nodes;
+vector<bool> visited;
+map m;
+
+const int INF = 100000000;
+int answer = INF;
+void DFS(Node& node, string& target, int depth)
+{
+        if (node.value == target)
+        {
+                answer = min(answer, depth);
+                return;
+        }
+
+        int key = m.get(node.value);
+        visited[key] = true;
+
+
+        for (auto child : node.childs)
+        {
+                int ckey = m.get(child->value);
+                if (visited[ckey] == false)
+                        DFS(*child, target, depth + 1);
+        }
+
+
+        visited[key] = false;
+}
+
+int solution(string begin, string target, vector<string> words) {
+        visited.resize(words.size());
+
+        words.push_back(begin);
+        words.push_back(target);
+
+        for (int i = 0; i < words.size(); i++)
+        {
+                Node node;
+                node.value = words[i];
+
+                nodes.push_back(node);
+                m.insert(words[i], i);
+        }
+
+        for (int i = 0; i < words.size(); i++)
+        {
+                for (int j = 0; j < words.size(); j++)
+                {
+                        nodes[i].Push(nodes[j]);
                 }
         }
 
-        return answer;
+        DFS(nodes[m.get(begin)], target, 0);
+
+        //    for (auto c : nodes[0].childs)
+          //      cout << c->value << " ";
+        return answer == INF ? 0 : answer;
 }
 
-// 0 1 4 5 6
-// c.size() - i == 4일 때
-// c[i] >= 4 여야하며
-// 
+// begin -> target
+// hit -> cog
+// words = {hot, dot, dog, lot, log, cog}
+// answer = hit -> hot -> dot -> dog -> cog
+
+// hit -> hot -> dot -> lot, dog -> log, cog
+
+// 모든 문자열을 순회하여 트리로 만들기 (무한 루프를 막기 위해 방문여부 체크)
+// DFS를 이용해서 모든 자식노드 순회
+// target을 만나면 end
+
 
