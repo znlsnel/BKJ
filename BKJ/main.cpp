eaa9b27b0fc4f2cb5ea3cec49b762ed6524be33e
Udi <dnel0605@gmail.com>
2024-06-28 11:17:26 +0900

    상담원 인원 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,60 +1,74 @@
 ﻿#include <string>
 #include <vector>
-#include <set>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+
 using namespace std;
 
-int N;
-bool canGoNextRound(set<int>& hands, set<int>& machs)
-{
-        for (int card : hands)
+vector<vector<int>> orders; // 분배가능한 모든 경우의 수
+
+// 모든 경우의 수를 구하는 함수(남은 인원, 유형, 컨테이너)
+void backtracking(int remain, int idx, vector<int> order) {
+        // 모든 인원을 담았으면 orders에 추가
+        if (remain > 0)
         {
-                int cntr = (N + 1) - card;
-                for (int mach : machs)
+                for (int i = idx; i < order.size(); i++)
                 {
-                        if (mach == cntr)
-                        {
-                                hands.erase(card);
-                                machs.erase(mach);
-                                return true;
-                        }
+                        order[i]++;
+                        backtracking(remain - 1, i, order);
+                        order[i]--;
                 }
+                return;
         }
-        return false;
+
+        orders.push_back(order);
 }
-int solution(int coin, vector<int> cards) {
-        int answer = 1;
-        N = cards.size();
 
-        set<int> hands;
-        set<int> draws;
-
-        for (int i = 0; i < cards.size() / 3; i++)
-                hands.insert(cards[i]);
+// 각 경우의 수에 맞춰 시뮬레이션을 돌림(순서,요청들)
+int simulation(vector<int> order, vector<vector<int>> reqs)
+{
+        //  priority_queue<int> q[order.size()];
+        priority_queue<int, vector<int>, greater<int>> q[order.size()];
 
 
-        for (int i = cards.size() / 3; i < cards.size(); i += 2)
+        for (int i = 0; i < order.size(); i++)
         {
-                draws.insert(cards[i]);
-                draws.insert(cards[i + 1]);
-
-                if (canGoNextRound(hands, hands))
-                {
-                        answer++;
-                }
-                else if (coin >= 1 && canGoNextRound(hands, draws))
-                {
-                        coin--;
-                        answer++;
-                }
-                else if (coin >= 2 && canGoNextRound(draws, draws))
-                {
-                        coin -= 2;
-                        answer++;
-                }
-                else
-                        break;
+                while (order[i]--)
+                        q[i].push(0);
         }
 
+        int result = 0;
+        for (auto req : reqs)
+        {
+                int start = req[0];
+                int time = req[1];
+                int type = req[2] - 1;
 
+                int mento = q[type].top();
+                q[type].pop();
+
+                if (mento <= start)
+                {
+                        q[type].push(start + time);
+                }
+                else if (mento > start)
+                {
+                        q[type].push(mento + time);
+                        result += mento - start;
+                }
+        }
+
+        return result;
+}
+int solution(int k, int n, vector<vector<int>> reqs) {
+        int answer = 99999999;
+
+        vector<int> kind(k, 1);
+        backtracking(n - k, 0, kind);
+
+        for (auto order : orders) {
+                answer = min(answer, simulation(order, reqs));
+        }
         return answer;
 }
