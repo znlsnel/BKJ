c14fa39e705546954d0e20fe067724152761ace1
Udi <dnel0605@gmail.com>
2024-06-30 13:28:40 +0900

    인사고과 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,44 +1,70 @@
 ﻿#include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
-
 using namespace std;
 
-long long solution(vector<int> sq) {
-        long long answer = 0;
+int solution(vector<vector<int>> scores) {
+        int answer = 1;
+        int a = scores[0][0];
+        int b = scores[0][1];
 
-        vector<int> v1(sq), v2(sq);
-        vector<long long> dp1(sq.size(), 0), dp2(sq.size(), 0);
-        long long max1 = 0, max2 = 0;
+        scores[0][0] = -10000;
+        scores[0][1] = -10000;
 
-        for (int i = 0; i < sq.size(); i++)
+        sort(scores.begin(), scores.end());
+        vector<int> startIdxs;
+
+        int start = scores[0][0];
+        for (int i = 1; i < scores.size(); i++) {
+                if (start != scores[i][0])
+                {
+                        start = scores[i][0];
+                        startIdxs.push_back(i - 1);
+                }
+        }
+        startIdxs.push_back(scores.size() - 1);
+
+        for (int i = 1; i < scores.size(); i++)
         {
-                if (i % 2 == 0)
-                        v1[i] *= -1;
-                else
-                        v2[i] *= -1;
+                int na = scores[i][0];
+                int nb = scores[i][1];
 
-                if (i == 0) {
-                        dp1[i] = v1[i];
-                        dp2[i] = v2[i];
-                        max1 = dp1[i];
-                        max2 = dp2[i];
+                scores[i][0] = -1000;
+                scores[i][1] = -1000;
+
+                if (na > a && nb > b)
+                        return -1;
+
+                // na, nb와 모든 것을 비교하여 이동이 가능한지 비교
+                // N^2 속도로 하면 불가능
+                // logN * N으로 해결
+
+                // 2,1  3,2  3,2  1,4
+                // 두 수의 합으로 scores 정렬(N Log N)
+                //  na + nb + 2 ~ 맨 뒤 탐색
+
+                int cnt = 10000;
+                bool flag = false;
+                int s = startIdxs.back();
+                for (int s = startIdxs.size() - 1; scores[startIdxs[s]][0] > na; s--, cnt--)
+                {
+                        if (scores[startIdxs[s]][1] > nb) {
+                                flag = true;
+                                break;
+                        }
+                        if (cnt == 0)
+                                break;
+                }
+                if (flag)
                         continue;
 
-                }
 
-                if (dp1[i - 1] + v1[i] > v1[i])
-                        dp1[i] = dp1[i - 1] + v1[i];
-                else
-                        dp1[i] = v1[i];
 
-                max1 = max(max1, dp1[i]);
-
-                if (dp2[i - 1] + v2[i] > v2[i])
-                        dp2[i] = dp2[i - 1] + v2[i];
-                else
-                        dp2[i] = v2[i];
-                max2 = max(max2, dp2[i]);
+                int nsum = na + nb;
+                if (nsum > a + b)
+                        answer++;
         }
-        return max(max1, max2);
+
+        return answer;
 }
