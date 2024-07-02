f3b9a23e23e52f2e5baa6b240b3fc9532f7e11f1
Udi <dnel0605@gmail.com>
2024-07-02 12:05:22 +0900

    숫자 타자 대회 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,55 +1,79 @@
 ﻿#include <string>
 #include <vector>
-#include <cmath>
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-vector<int> solution(int e, vector<int> starts) {
-        vector<int> dp(e + 1), answer;
+inline int GetCost(int y, int x, int dy, int dx)
+{
+        if (y == dy && x == dx)
+                return 1;
 
-        for (int i = 1; i <= e; i++)
-        {
-                for (int j = 1; j <= e; j++)
-                {
-                        if (i * j > e)
-                                break;
-                        dp[i * j]++;
-                }
-        }
+        int dirY = abs(dy - y);
+        int dirX = abs(dx - x);
 
-        int maxId = e;
-        int maxV = dp[e];
-        for (int i = e; i >= 0; i--)
-        {
-                if (dp[i] >= maxV)
-                {
-                        maxId = i;
-                        maxV = dp[i];
-                }
+        int d = min(dirY, dirX);
+        int d2 = max(dirY, dirX) - d;
 
-                dp[i] = maxId;
-        }
-
-        for (int idx : starts)
-                answer.push_back(dp[idx]);
-
-        //  for (int n : dp)
-            //  cout << n <<" ";
-
-        return answer;
+        return d * 3 + d2 * 2;
 }
 
-// arr[i][j] = 2 + arr[i/2][j/2];
-// arr[i] = 
-// 0 - 0
-// 1 - 1
-// 2 - 2
-// 3 - 2
-// 4 - 3
-// 5 - 2
-// 6 - 4
+inline void ctop(char c, int& ny, int& nx)
+{
+        //   cout << c << "\n";
+        if (c > '0' && c <= '9')
+        {
+                int a = (c - '1');
+                ny = a / 3;
+                nx = a % 3;
+        }
+        else if (c == '0') {
+                ny = 3;
+                nx = 1;
+        }
+        else if (c == '*')
+        {
+                ny = 3;
+                nx = 0;
+        }
+        else if (c == '#')
+        {
+                ny = 3;
+                nx = 2;
+        }
+}
 
+int dp[100001][12][12];
+int DFS(const string& nums, int idx, int ly, int lx, int ry, int rx)
+{
+        if (idx == nums.size())
+                return 0;
 
-// 1 2 2 3 2 4
+        if (ly == ry && lx == rx)
+                return 9999999;
 
+        int lnum = ly * 3 + lx + 1;
+        int rnum = ry * 3 + rx + 1;
+        if (dp[idx][lnum][rnum] != 0)
+                return dp[idx][lnum][rnum];
+
+        int ny, nx;
+        ctop(nums[idx], ny, nx);
+        int cost_1 = GetCost(ly, lx, ny, nx);
+        int cost_2 = GetCost(ry, rx, ny, nx);
+
+        int p1 = cost_1 + DFS(nums, idx + 1, ny, nx, ry, rx);
+        int p2 = cost_2 + DFS(nums, idx + 1, ly, lx, ny, nx);
+
+        return dp[idx][lnum][rnum] = min(p1, p2);
+}
+
+int solution(string numbers) {
+        return DFS(numbers, 0, 1, 0, 1, 2);
+}
+
+// 제자리 1
+// 상하좌우 2
+// 대각 3
