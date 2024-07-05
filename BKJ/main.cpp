16b3eb857ecf86bb0e36c8c86ee3fa6a79383928
Udi <dnel0605@gmail.com>
2024-07-05 12:14:55 +0900

    부대복귀 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,64 +1,45 @@
 ﻿#include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
-vector<vector<int>> lights;
-vector<bool> isLighting;
-int answer = 0;
 
-void DFS(int idx, int parent)
+void dijkstra(vector<vector<int>>& roads, vector<int>& dists, int start)
 {
-        for (int& n : lights[idx])
-        {
-                if (n != parent)
-                {
-                        DFS(n, idx);
+        dists[start] = 0;
 
-                        if (!isLighting[idx] && !isLighting[n]) {
-                                isLighting[idx] = true;
-                                answer++;
+        priority_queue<pair<int, int>> q;
+        q.push({ 0, start });
+
+        while (!q.empty())
+        {
+                auto [cost, node] = q.top(); q.pop();
+                cost *= -1;
+
+                for (int& next : roads[node])
+                {
+                        if (dists[next] == -1 || dists[next] > cost + 1)
+                        {
+                                dists[next] = cost + 1;
+                                q.push({ -(cost + 1), next });
                         }
                 }
         }
 }
 
-int solution(int n, vector<vector<int>> lighthouse) {
-        lights.resize(n + 1);
-        isLighting.resize(n + 1);
-        for (auto& l : lighthouse)
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+        vector<vector<int>> _roads(n + 1);
+        for (auto& r : roads)
         {
-                int a = l[0];
-                int b = l[1];
-                lights[a].push_back(b);
-                lights[b].push_back(a);
+                _roads[r[0]].push_back(r[1]);
+                _roads[r[1]].push_back(r[0]);
         }
-        DFS(1, 0);
-        return answer;
+
+        vector<int> dists(n + 1, -1);
+        dijkstra(_roads, dists, destination);
+
+        for (int& s : sources)
+                s = dists[s];
+
+        return sources;
 }
-
-// 1 - 2 3 4 5
-// 2 - 1 9
-// 3 - 1
-// 4 - 1
-// 5 - 1 6
-// 6 - 5 7 8
-// 7 - 6
-// 8 - 6
-// 9 - 2 10
-// 10 - 9
-
-// 조건 1. 2개 이상의 노드와 연결된 애들
-// 조건 2. 옆에 켜져있는 등대가 있으면 안됨
-
-// 
-// priority_queue<연결된 노드 수, index> q;
-// vector<bool> visited;
-// if (q.top이 등대로 선정된 노드와 연결 되어 있다면)
-//      return;
-// 아니면 등대로 지정
-// 핵심은 등대로 선정된 노드와 연결 되어 있는지 어떻게 확인하는가?
-// 
-// 
