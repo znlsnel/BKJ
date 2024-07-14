56b6caf96b919b3ad2faa998ffac7db1a462773d
Udi <dnel0605@gmail.com>
2024-07-14 16:10:21 +0900

    다단계 칫솔 판매

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,59 +1,97 @@
-﻿#include <string>
-#include <vector>
+﻿#include <string>
+#include <vector>
+#include <iostream>
 
-using namespace std;
+using namespace std;
 
-int Find_Last_Zero(string Str)
+struct map
 {
-	for (int i  = Str.length() - 1; i  >= 0; i--)
-	{
-		if (Str[i] == '0') return i;
-	}
-	return  - 1;
+        map(int size = 1000000)
+        {
+                table.resize(size);
+        }
+
+        int GetHash(string str)
+        {
+                int ret = 1;
+                for (int i = 0; i < str.size(); i++)
+                {
+                        ret += str[i] * (i + 1);
+                        ret %= table.size();
+                }
+                return ret;
+        }
+
+
+
+        void insert(string key, int idx)
+        {
+                int k = GetHash(key);
+                table[k].push_back({ key, idx });
+        }
+
+        int GetValue(string key)
+        {
+                int k = GetHash(key);
+
+                for (auto& v : table[k])
+                {
+                        if (v.first == key)
+                        {
+                                return v.second;
+                        }
+                }
+
+                return -1;
+        }
+
+        vector<vector<pair<string, int>>> table;
+};
+
+// 이름, 트리, (판매자, 판매개수)
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> sellers, vector<int> amounts) {
+
+        map _map;
+        vector<int> parents(enroll.size(), -1);
+
+        for (int i = 0; i < enroll.size(); i++)
+                _map.insert(enroll[i], i);
+
+        for (int i = 0; i < referral.size(); i++)
+        {
+                if (referral[i] == "-")
+                        continue;
+
+                int parent = _map.GetValue(referral[i]);
+                parents[i] = parent;
+        }
+
+        vector<int> answer(enroll.size());
+        for (int i = 0; i < sellers.size(); i++)
+        {
+                int seller = _map.GetValue(sellers[i]);
+                int amount = amounts[i] * 100;
+
+                while (seller >= 0)
+                {
+                        int profit = amount;
+                        if (amount >= 10)
+                        {
+                                // 12 의 10% == 1
+
+                                profit = amount - (amount / 10);
+                        }
+
+                        // cout << seller << ", " << amount <<" - " <<profit << "\n";
+
+                        answer[seller] += profit;
+                        amount -= profit;
+                        seller = parents[seller];
+                }
+
+        }
+
+        return answer;
 }
 
-vector<string> solution(vector<string> s)
-{
-	vector<string> answer;
-	for (int i  = 0; i  < s.size(); i++)
-	{
-		string Str  = "";
-		int Cnt  = 0;
-		for (int j  = 0; j  < s[i].length(); j++)
-		{
-			Str  += s[i][j];
-			if (Str.length() >= 3)
-			{
-				if (Str.substr(Str.length() - 3, 3) == "110")
-				{
-					Cnt++;
-					Str.erase(Str.length() - 3, Str.length());
-				}
-			}
-		}
-
-		int LZI  = Find_Last_Zero(Str);
-		if (LZI  == -1)
-		{
-			string Result  = "";
-			while (Cnt--) Result  += "110";
-			Result  += Str;
-			answer.push_back(Result);
-		}
-	else
-	{
-		string Result  = "";
-		for (int j  = 0; j  < Str.length(); j++)
-		{
-			if (j  == LZI)
-			{
-				Result  += Str[j];
-				while (Cnt--) Result  += "110";
-			}
-			else Result  += Str[j];
-		}
-		answer.push_back(Result);
-	}
-	}
-	return answer;
-}
+// string에 index를 부여하는게 핵심 문제
