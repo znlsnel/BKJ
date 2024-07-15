4a27df62ec242bca93a89c89c94f0b6d31a0c74b
Udi <dnel0605@gmail.com>
2024-07-15 10:43:46 +0900

    모두 0으로 만들기 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,97 +1,36 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-struct map
-{
-        map(int size = 1000000)
-        {
-                table.resize(size);
-        }
+long long answer = 0;
+// now : 현재 방문 중인 노드,  parent : 바로 이전 노드 (= 부모 노드)
+void DFS(vector<vector<int>>& graph, vector<long long>& sum, int now, int parent) {
+        for (int i = 0; i < graph[now].size(); ++i)
+                if (graph[now][i] != parent)
+                        DFS(graph, sum, graph[now][i], now);
 
-        int GetHash(string str)
-        {
-                int ret = 1;
-                for (int i = 0; i < str.size(); i++)
-                {
-                        ret += str[i] * (i + 1);
-                        ret %= table.size();
-                }
-                return ret;
-        }
-
-
-
-        void insert(string key, int idx)
-        {
-                int k = GetHash(key);
-                table[k].push_back({ key, idx });
-        }
-
-        int GetValue(string key)
-        {
-                int k = GetHash(key);
-
-                for (auto& v : table[k])
-                {
-                        if (v.first == key)
-                        {
-                                return v.second;
-                        }
-                }
-
-                return -1;
-        }
-
-        vector<vector<pair<string, int>>> table;
-};
-
-// 이름, 트리, (판매자, 판매개수)
-vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> sellers, vector<int> amounts) {
-
-        map _map;
-        vector<int> parents(enroll.size(), -1);
-
-        for (int i = 0; i < enroll.size(); i++)
-                _map.insert(enroll[i], i);
-
-        for (int i = 0; i < referral.size(); i++)
-        {
-                if (referral[i] == "-")
-                        continue;
-
-                int parent = _map.GetValue(referral[i]);
-                parents[i] = parent;
-        }
-
-        vector<int> answer(enroll.size());
-        for (int i = 0; i < sellers.size(); i++)
-        {
-                int seller = _map.GetValue(sellers[i]);
-                int amount = amounts[i] * 100;
-
-                while (seller >= 0)
-                {
-                        int profit = amount;
-                        if (amount >= 10)
-                        {
-                                // 12 의 10% == 1
-
-                                profit = amount - (amount / 10);
-                        }
-
-                        // cout << seller << ", " << amount <<" - " <<profit << "\n";
-
-                        answer[seller] += profit;
-                        amount -= profit;
-                        seller = parents[seller];
-                }
-
-        }
-
-        return answer;
+        sum[parent] += sum[now];
+        answer += abs(sum[now]);
 }
 
-// string에 index를 부여하는게 핵심 문제
+long long solution(vector<int> a, vector<vector<int>> edges) {
+
+        vector<long long> sum(a.size());
+
+        for (int i = 0; i < a.size(); ++i)
+                sum[i] = a[i];
+
+        vector<vector<int>> graph(a.size());
+        for (int i = 0; i < edges.size(); ++i) {
+                graph[edges[i][0]].push_back(edges[i][1]);
+                graph[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        DFS(graph, sum, 0, 0);
+
+        if (sum[0] == 0)
+                return answer;
+        else
+                return -1;
+}
