5dab323f70b0499648ec2b650f3dae5c65685044
Udi <dnel0605@gmail.com>
2024-07-19 11:46:29 +0900

    경주로 건설 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,31 +1,58 @@
-﻿#include <vector>
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <iostream>
 using namespace std;
 
-int solution(vector<int> a) {
-        int answer = 0;
-        int len = a.size();
-        vector<int> left(len);
-        vector<int> right(len);
+bool CHECK(vector<vector<int>>& board, int y, int x)
+{
+        return y >= 0 && y < board.size() && x >= 0 && x < board[y].size() && board[y][x] == 0;
+}
 
-        int MIN = a[0];
-        for (int i = 0; i < len; i++)
+int dy[4] = { 1, 0, -1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+int dijkstra(vector<vector<int>>& board)
+{
+        // cost, node, type (아래 - 0, 옆 - 1);
+        vector<vector<int>> dist(board.size(), vector<int>(board[0].size(), 1000000000));
+        priority_queue<tuple<int, int, int, int>> q;
+        q.push({ 0, 0, 0, 0 });
+        q.push({ 0, 0, 0, 1 });
+        dist[0][0] = 0;
+
+        while (!q.empty())
         {
-                if (MIN > a[i])
-                        MIN = a[i];
+                auto [cost, cy, cx, type] = q.top(); q.pop();
+                //  cout << cy << " " << cx << "--- cost : " << cost <<", type : " <<type << "\n";
 
-                left[i] = MIN;
-        }
+                for (int i = 0; i < 4; i++)
+                {
+                        int ny = cy + dy[i];
+                        int nx = cx + dx[i];
+                        if (!CHECK(board, ny, nx))
+                                continue;
 
-        MIN = a[len - 1];
-        for (int i = len - 1; i >= 0; i--) {
-                if (MIN > a[i]) MIN = a[i];
-                right[i] = MIN;
-        }
+                        int nt = ny == cy ? 1 : 0;
+                        int nc = nt == type ? cost + 100 : cost + 600;
 
-        for (int i = 0; i < len; i++)
-        {
-                if (a[i] <= left[i] || a[i] <= right[i])
-                        answer++;
+                        if (dist[ny][nx] >= nc)
+                        {
+                                dist[ny][nx] = nc;
+                                q.push({ nc, ny, nx, nt });
+                        }
+                }
         }
-        return answer;
+        int y = board.size() - 1;
+        int x = board[0].size() - 1;
+        return dist[y][x];
+}
+
+// 0 0 1 0
+// 0 0 0 0
+// 0 1 0 1
+// 1 0 0 0
+
+int solution(vector<vector<int>> board) {
+        return dijkstra(board);
 }
