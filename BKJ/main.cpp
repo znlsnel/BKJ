33cdd0b8a282742b94d01919008a065d9fa64788
Udi <dnel0605@gmail.com>
2024-07-20 23:09:11 +0900

    보석 쇼핑 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,68 +1,111 @@
 ﻿#include <string>
 #include <vector>
-#include <queue>
-#include <tuple>
+#include <set>
 #include <iostream>
+
 using namespace std;
 
-bool CHECK(vector<vector<int>>& board, int y, int x)
+struct MAP
 {
-        return y >= 0 && y < board.size() && x >= 0 && x < board[y].size() && board[y][x] == 0;
-}
-
-int dy[4] = { 1, 0, -1, 0 };
-int dx[4] = { 0, 1, 0, -1 };
-int dijkstra(vector<vector<int>>& board)
-{
-        // cost, node, type (아래 - 0, 옆 - 1);
-        vector<vector<int>> dist0(board.size(), vector<int>(board[0].size(), 1000000000));
-        vector<vector<int>> dist1(board.size(), vector<int>(board[0].size(), 1000000000));
-
-        priority_queue<tuple<int, int, int, int>> q;
-        q.push({ 0, 0, 0, 0 });
-        q.push({ 0, 0, 0, 1 });
-        dist0[0][0] = 0;
-        dist1[0][0] = 0;
-
-
-        while (!q.empty())
+        MAP(int size = 1000000)
         {
-                auto [cost, cy, cx, type] = q.top(); q.pop();
-                cost *= -1;
-
-                for (int i = 0; i < 4; i++)
-                {
-                        int ny = cy + dy[i];
-                        int nx = cx + dx[i];
-                        if (!CHECK(board, ny, nx))
-                                continue;
-
-                        int nextType = ny == cy ? 1 : 0;
-                        int nextCost = nextType == type ? cost + 100 : cost + 600;
-
-                        if (nextType == 0 && dist0[ny][nx] > nextCost)
-                        {
-                                dist0[ny][nx] = nextCost;
-                                q.push({ -(nextCost), ny, nx, nextType });
-                        }
-                        else if (nextType == 1 && dist1[ny][nx] > nextCost)
-                        {
-                                dist1[ny][nx] = nextCost;
-                                q.push({ -(nextCost), ny, nx, nextType });
-                        }
-                }
+                table.resize(size);
         }
 
-        int y = board.size() - 1;
-        int x = board[0].size() - 1;
-        return min(dist0[y][x], dist1[y][x]);
-}
+        int Hash(string str)
+        {
+                int ret = 1;
+                for (char& c : str)
+                {
+                        ret *= c - 'A';
+                        ret %= table.size();
+                }
+                return ret;
+        }
 
-// 0 0 1 0
-// 0 0 0 0
-// 0 1 0 1
-// 1 0 0 0
+        bool Insert(string key, int value)
+        {
+                int hash = Hash(key);
+                for (auto& t : table[hash])
+                {
+                        if (t.first == key)
+                                return false;
+                }
 
-int solution(vector<vector<int>> board) {
-        return dijkstra(board);
+                table[hash].push_back({ key, value });
+                return true;
+        }
+
+        int GetValue(string key)
+        {
+                int hash = Hash(key);
+                for (auto& t : table[hash])
+                {
+                        if (t.first == key)
+                                return t.second;
+                }
+
+                return -1;
+        }
+
+        vector<vector<pair<string, int>>> table;
+};
+
+vector<int> solution(vector<string> gems) {
+
+        MAP map;
+        int gemCnt = 0;
+        for (string& str : gems)
+        {
+                if (map.Insert(str, gemCnt))
+                        gemCnt++;
+        }
+        vector<int> haveGem(gemCnt);
+        vector<bool> foundGem(gemCnt);
+
+        int foundGemCnt = 0;
+        int s = 0;
+        int e = 0;
+
+        for (int i = 0; i < gems.size(); i++)
+        {
+                if (foundGemCnt >= gemCnt)
+                        break;
+
+                int idx = map.GetValue(gems[i]);
+                haveGem[idx]++;
+
+                if (!foundGem[idx])
+                {
+                        foundGem[idx] = true;
+                        foundGemCnt++;
+                }
+                e = i;
+        }
+        vector<int> answer{ s, e };
+
+        for (int i = e + 1; i < gems.size(); i++)
+        {
+                string& gem = gems[i];
+
+                int sidx = map.GetValue(gems[s]);
+                while (haveGem[sidx] > 1)
+                {
+                        haveGem[sidx]--;
+                        s++;
+                        sidx = map.GetValue(gems[s]);
+                }
+
+                if (answer[1] - answer[0] > e - s)
+                {
+                        answer[0] = s;
+                        answer[1] = e;
+                }
+
+                int idx = map.GetValue(gem);
+                haveGem[idx]++;
+                e = i;
+        }
+
+        return { answer[0] + 1, answer[1] + 1 };
 }
