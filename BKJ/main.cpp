45328f4818e9f0db5066ef03ec5b69fd342d50bd
Udi <dnel0605@gmail.com>
2024-07-27 16:57:26 +0900

    정수 삼각형 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,162 +1,24 @@
 ﻿#include <string>
 #include <vector>
 #include <iostream>
-#include <memory>
 
 using namespace std;
 
-template<typename T>
-struct priority_queue
-{
-        priority_queue(bool b = false) : isDescOrder(b) {}
+int dp[501][501];
 
-        void insert(T value)
+int solution(vector<vector<int>> triangle) {
+        fill(&dp[0][0], &dp[500][500], -1);
+        int n = triangle.size();
+
+        for (int i = 0; i < triangle.size(); i++)
+                dp[n - 1][i] = triangle[n - 1][i];
+
+        for (int i = n - 2; i >= 0; i--)
         {
-                int idx = data.size();
-                data.push_back(value);
-
-                while (idx > 0)
+                for (int j = 0; j <= i; j++)
                 {
-                        int parent = (idx - 1) / 2;
-                        if (!CMP(data[idx], data[parent]))
-                        {
-                                break;
-                        }
-                        swap(data[idx], data[parent]);
-
-                        idx = parent;
-                }
-
-        }
-
-        void pop()
-        {
-                if (data.empty())
-                        return;
-
-                int idx = 0;
-                data[idx] = data.back();
-                data.pop_back();
-
-                while (idx < data.size())
-                {
-                        int left = (idx * 2) + 1;
-                        int right = left + 1;
-
-                        if (left >= data.size())
-                                break;
-
-                        if (right < data.size() && CMP(data[right], data[left]))
-                                left = right;
-
-                        if (!CMP(data[left], data[idx]))
-                                break;
-
-                        swap(data[left], data[idx]);
-                        idx = left;
+                        dp[i][j] = triangle[i][j] + max(dp[i + 1][j], dp[i + 1][j + 1]);
                 }
         }
-
-        bool CMP(const T& a, const T& b)
-        {
-                if (isDescOrder)
-                        return *a > *b;
-
-                else
-                        return *a < *b;
-        }
-
-        T top()
-        {
-                return data.empty() == true ? nullptr : data[0];
-        }
-
-        bool empty()
-        {
-                return data.empty();
-        }
-
-        bool isDescOrder = false;
-        vector<T> data;
-};
-
-struct NODE
-{
-        bool use = false;
-        int value;
-
-        bool operator < (NODE other)
-        {
-                return value < other.value;
-        }
-
-        bool operator > (NODE other)
-        {
-                return value > other.value;
-        }
-
-        NODE(int v) : value(v) { use = false; };
-};
-
-vector<int> solution(vector<string> operations) {
-        priority_queue<NODE*> minQ;
-        priority_queue<NODE*> maxQ(true);
-        vector<unique_ptr<NODE>> nodes;
-
-        for (string& str : operations)
-        {
-                int value = stoi(str.substr(2));
-
-                if (str[0] == 'I')
-                {
-                        nodes.push_back(make_unique<NODE>(value));
-                        NODE* node = nodes.back().get();
-
-                        minQ.insert(node);
-                        maxQ.insert(node);
-                }
-                else if (value == 1)
-                {
-                        while (!maxQ.empty() && maxQ.top()->use)
-                                maxQ.pop();
-
-
-                        if (!maxQ.empty())
-                        {
-                                NODE* node = maxQ.top();
-                                node->use = true;
-                                maxQ.pop();
-                        }
-
-                }
-                else if (value == -1)
-                {
-                        while (!minQ.empty() && minQ.top()->use)
-                                minQ.pop();
-
-                        if (!minQ.empty())
-                        {
-                                NODE* node = minQ.top();
-                                node->use = true;
-                                minQ.pop();
-                        }
-                }
-
-        }
-
-        while (!maxQ.empty() && maxQ.top()->use == true)
-                maxQ.pop();
-
-        while (!minQ.empty() && minQ.top()->use == true)
-                minQ.pop();
-
-        int minTop = minQ.empty() ? 0 : minQ.top()->value;
-        int maxTop = maxQ.empty() ? 0 : maxQ.top()->value;
-        return { maxTop, minTop };
+        return dp[0][0];
 }
-
-// ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
-// -5643
-
-// min   -45 45 333
-// max   333 45 -45
