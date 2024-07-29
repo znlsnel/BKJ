923492d1356fba931df49c983f19c24f75b492fd
Udi <dnel0605@gmail.com>
2024-07-29 16:01:50 +0900

    야근 지수 [Lv.3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,41 +1,29 @@
 ﻿#include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-vector<bool> visited;
-vector<vector<int>> v;
+long long solution(int n, vector<int> works) {
 
-void DFS(int node)
-{
-        visited[node] = true;
-        for (int next : v[node])
+        priority_queue<int> q;
+        for (int& n : works)
+                q.push(n);
+
+        while (n--)
         {
-                if (visited[next])
+                int maxN = q.top(); q.pop();
+                q.push(maxN - 1);
+        }
+
+        long long answer = 0;
+        while (!q.empty())
+        {
+                int n = q.top(); q.pop();
+                if (n <= 0)
                         continue;
-                DFS(next);
-        }
-}
 
-int solution(int n, vector<vector<int>> computers) {
-        visited.resize(n);
-        v.resize(n);
-        for (int i = 0; i < computers.size(); i++)
-        {
-                for (int j = 0; j < computers[i].size(); j++)
-                {
-                        if (i == j || computers[i][j] == 0)
-                                continue;
-                        v[i].push_back(j);
-                }
-        }
-        int answer = 0;
-        for (int i = 0; i < n; i++)
-        {
-                if (!visited[i]) {
-                        DFS(i);
-                        answer++;
-                }
+                answer += n * n;
         }
 
         return answer;
