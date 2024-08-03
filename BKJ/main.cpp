c961bfcf9566e777543019e08614515bcda10d89
Udi <dnel0605@gmail.com>
2024-08-04 02:31:57 +0900

    상담원 인원 [Lv.3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,27 +1,72 @@
 ﻿#include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
-
+#include <queue>
 
 using namespace std;
 
-int solution(vector<int> A, vector<int> B) {
+vector<vector<int>> orders;
+void backtracking(vector<int> mentos, int idx, int coin)
+{
+        if (coin == 0) {
+                orders.push_back(mentos);
+                return;
+        }
 
-        sort(A.begin(), A.end(), [](int& a, int& b) {return a > b; });
-        sort(B.begin(), B.end(), [](int& a, int& b) {return a > b; });
-
-        int answer = 0;
-        for (int idx_a = 0, idx_b = 0; idx_a < A.size(); idx_a++)
+        for (int i = idx; i < mentos.size(); i++)
         {
-                int numA = A[idx_a];
-                int numB = B[idx_b];
+                mentos[i]++;
+                backtracking(mentos, i, coin - 1);
+                mentos[i]--;
+        }
+}
 
-                if (numA < numB)
+int Counseling(vector<int>& order, vector<vector<int>>& reqs)
+{
+        priority_queue<int, vector<int>, greater<int>> q[order.size()];
+
+        for (int i = 0; i < order.size(); i++)
+        {
+                while (order[i]-- > 0)
+                        q[i].push(0);
+
+        }
+
+
+
+        int ret = 0;
+        for (vector<int>& req : reqs)
+        {
+                int start = req[0];
+                int time = req[1];
+                int type = req[2] - 1;
+
+                int mento = q[type].top(); q[type].pop();
+
+                if (mento > start)
                 {
-                        answer++;
-                        idx_b++;
+                        ret += mento - start;
+                        q[type].push(mento + time);
+                }
+                else if (mento <= start)
+                {
+                        q[type].push(start + time);
                 }
         }
+
+
+
+        return ret;
+}
+
+int solution(int k, int n, vector<vector<int>> reqs) {
+        vector<int> mentos(k, 1);
+        backtracking(mentos, 0, n - k);
+
+
+        int answer = Counseling(orders[0], reqs);
+        for (int i = 1; i < orders.size(); i++)
+                answer = min(answer, Counseling(orders[i], reqs));
+
         return answer;
 }
