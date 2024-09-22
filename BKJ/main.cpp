ca4d80ab0b30ad595f004112f3f0653126a2ae81
Udi <dnel0605@gmail.com>
2024-09-22 21:37:09 +0900

    거스름돈 [Lv.3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,78 +1,24 @@
 ﻿#include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
-
 
 using namespace std;
 
-int ttoi(string s)
+int solution(int n, vector<int> money)
 {
-        int ret = 0;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
 
-        ret += (s[4] - '0');
-        ret += (s[3] - '0') * 10;
-
-        ret += (s[1] - '0') * 60;
-        ret += (s[0] - '0') * 600;
-
-        return ret;
-}
-
-string itot(int time)
-{
-        string ret = "";
-        ret += to_string(time / 600);
-        ret += to_string(time % 600 / 60);
-        ret += ':';
-        ret += to_string(time % 600 % 60 / 10);
-        ret += to_string(time % 600 % 60 % 10);
-
-        return ret;
-}
-
-string solution(int n, int t, int m, vector<string> timetable) {
-
-        vector<int> times;
-
-        for (string& t : timetable)
-                times.push_back(ttoi(t));
-
-        sort(times.begin(), times.end());
-
-        int curBus = ttoi("09:00");
-        int lastBus = curBus + (n - 1) * t;
-
-        int idx = 0, seatsLeft = m;
-
-        // 마지막 버스까지 계속 처리
-        for (int i = 0; i < n; ++i) {
-                int seatsLeft = m;
-
-                while (seatsLeft > 0 && idx < times.size() && times[idx] <= curBus) {
-                        seatsLeft--;
-                        idx++;
-                }
-
-                // 마지막 버스일 때
-                if (i == n - 1)
+        // 1, 2, 5
+        for (int coin : money)
+        {
+                for (int i = coin; i <= n; ++i)
                 {
-                        // 자리가 남으면 마지막 버스 시간에 탑승 가능
-                        if (seatsLeft > 0)
-                        {
-                                return itot(curBus);
-                        }
-
-                        // 자리가 없으면 마지막으로 탄 사람보다 1분 빨리
-                        else
-                        {
-                                return itot(times[idx - 1] - 1);
-                        }
+                        cout << i << " += " << dp[i - coin] << "(" << i - coin << ")\n";
+                        dp[i] += dp[i - coin];
                 }
-
-                curBus += t;  // 다음 버스 시간으로 갱신
         }
 
-        return itot(lastBus);
 
+        return dp[n] % 1000000007;
 }
