bad2a6796238b686bc20b1e338e1ddfa2cbd5599
Udi <dnel0605@gmail.com>
2024-09-28 22:25:12 +0900

    보행자 천국 [Lv.3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,59 +1,50 @@
-﻿#include <string>
-#include <vector>
-#include <algorithm>
+﻿#include <vector>
+#include <iostream>
 
 using namespace std;
 
-// 현재 구조물이 유효한지 확인하는 함수
-bool isValid(vector<vector<int>>& build) {
-        for (auto& b : build) {
-                int x = b[0], y = b[1], a = b[2];
-                if (a == 0) { // 기둥
-                        if (y == 0 || // 바닥 위에 있거나
-                                find(build.begin(), build.end(), vector<int>{x - 1, y, 1}) != build.end() || // 왼쪽에 보가 있거나
-                                find(build.begin(), build.end(), vector<int>{x, y, 1}) != build.end() || // 오른쪽에 보가 있거나
-                                find(build.begin(), build.end(), vector<int>{x, y - 1, 0}) != build.end()) { // 아래에 기둥이 있거나
+int MOD = 20170805;
+
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int m, int n, vector<vector<int>> city_map) {
+
+        vector<vector<int>> right(m, vector<int>(n, 0));
+        vector<vector<int>> down(m, vector<int>(n, 0));
+
+        right[0][1] = 1;
+        down[1][0] = 1;
+
+
+        for (int i = 0; i < m; i++)
+        {
+                for (int j = 0; j < n; j++)
+                {
+                        if (city_map[i][j] == 1)
                                 continue;
-                        }
-                        else {
-                                return false;
-                        }
-                }
-                else { // 보
-                        if (find(build.begin(), build.end(), vector<int>{x, y - 1, 0}) != build.end() || // 왼쪽 아래에 기둥이 있거나
-                                find(build.begin(), build.end(), vector<int>{x + 1, y - 1, 0}) != build.end() || // 오른쪽 아래에 기둥이 있거나
-                                (find(build.begin(), build.end(), vector<int>{x - 1, y, 1}) != build.end() &&
-                                        find(build.begin(), build.end(), vector<int>{x + 1, y, 1}) != build.end())) { // 양쪽에 보가 있거나
-                                continue;
-                        }
-                        else {
-                                return false;
-                        }
-                }
-        }
-        return true;
-}
 
-vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
-        vector<vector<int>> build;
+                        else if (city_map[i][j] == 2)
+                        {
+                                if (i < m - 1 && i > 0)
+                                        down[i + 1][j] = down[i][j] % MOD;
 
-        for (auto& frame : build_frame) {
-                int x = frame[0], y = frame[1], a = frame[2], b = frame[3];
-
-                if (b == 1) { // 설치
-                        build.push_back({ x, y, a });
-                        if (!isValid(build)) { // 설치 후 유효하지 않으면 취소
-                                build.pop_back();
+                                if (j < n - 1 && j > 0)
+                                        right[i][j + 1] += right[i][j] % MOD;
                         }
-                }
-                else { // 제거
-                        build.erase(remove(build.begin(), build.end(), vector<int>{x, y, a}), build.end());
-                        if (!isValid(build)) { // 제거 후 유효하지 않으면 다시 설치
-                                build.push_back({ x, y, a });
+                        else
+                        {
+                                if (i < m - 1)
+                                        down[i + 1][j] += (down[i][j] + right[i][j]) % MOD;
+
+                                if (j < n - 1)
+                                        right[i][j + 1] += (down[i][j] + right[i][j]) % MOD;
                         }
                 }
         }
 
-        sort(build.begin(), build.end()); // 최종 결과를 정렬
-        return build;
+        return (right[m - 1][n - 1] + down[m - 1][n - 1]) % MOD;
 }
+
+// 0 = 통행가능
+// 1 = 동행불가능
+// 2 = 직진만 가능
