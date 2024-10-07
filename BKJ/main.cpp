b291c9d70dddcdd7a5cbe4e71f38197f1b05d19a
Udi <dnel0605@gmail.com>
2024-10-07 11:24:22 +0900

    리틀 프렌즈 사천성 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,166 +1,124 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
-#include <set>
+#include <queue>
 
 using namespace std;
 
-bool Compute(int a, int b, int& result, int base, bool plus)
-{
-        int thirdDigit = 0;
-        int secondDigit = (a / 10) + (b / 10);
-        int firstDigit = (a % 10) + (b % 10);
+typedef struct qinfo {
+        //행, 열, 방향, 꺾은 횟수
+        int x, y, dir, cnt;
+};
+typedef struct ainfo {
+        int x, y;
+};
 
-        if (plus)
-        {
-                if (firstDigit / base > 0)
-                {
-                        secondDigit++;
-                        firstDigit -= base;
-                }
+int pairCnt = 0;
+int N = 0, M = 0;
 
-                if (secondDigit / base > 0)
-                {
-                        thirdDigit++;
-                        secondDigit -= base;
-                }
-        }
-        else
-        {
-                secondDigit = (a / 10) - (b / 10); // 무조건 0보다 큼
-                firstDigit = (a % 10) - (b % 10);
-                if (firstDigit < 0) {
-                        secondDigit--;
-                        firstDigit = (firstDigit % base + base) % base;
-                }
-        }
-
-
-
-        int total = thirdDigit * 100 + secondDigit * 10 + firstDigit;
-        char p = plus ? '+' : '-';
-        //   cout << "[" << base << "] " << a << " "<< p <<" " << b << " = " << total << "\n";
-
-        if (result == -1)
-                result = total;
-
-        return total == result;
+bool inRange(int x, int y) {
+        return (0 <= x && x < M && 0 <= y && y < N);
 }
 
-void Parsing(string& exprs, int& a, int& b, int& result, bool& plus)
-{
-        a = exprs[0] - '0';
-        int next = 2;
+bool BFS(int start_x, int start_y, int end_x, int end_y, vector<string>& boards) {
 
-        if (exprs[1] != ' ') {
-                a = a * 10 + exprs[1] - '0';
-                next++;
-        }
+        pair<int, int> nextdir[4] = { {1,0},{0,1},{-1,0},{0,-1} };
 
-        plus = false;
-        if (exprs[next] == '+')
-                plus = true;
+        queue<qinfo> q;
 
-        next += 2;
+        //어떤 방향으로 출발해도 상관 없다는 표시로 dir = -1;
+        q.push({ start_x, start_y, -1, 0 });
 
-        b = exprs[next] - '0';
-        if (exprs[next + 1] != ' ') {
-                b = b * 10 + exprs[next + 1] - '0';
-                next++;
-        }
+        while (!q.empty()) {
 
-        next += 4;
+                int now_x = q.front().x; int now_y = q.front().y;
+                int cnt = q.front().cnt; int dir = q.front().dir;
+                q.pop();
 
+                for (int i = 0; i < 4; i++) {
 
-        result = exprs[next] - '0';
-        if (exprs[next] == 'X')
-                result = -1;
-
-        next++;
-        while (next < exprs.size())
-        {
-                result = result * 10 + exprs[next] - '0';
-                next++;
-        }
-
-        //  cout << "A : " << a << " B : " << b << " result : " << result << " plus : " << plus << "\n";
-}
-
-vector<string> solution(vector<string> expressions) {
-        set<int> questions;
-        set<int> bases;
-
-        // 진법 범위 찾기
-        int start = 2;
-        for (int i = 0; i < expressions.size(); i++)
-        {
-                string& exprs = expressions[i];
-
-                if (exprs.back() == 'X')
-                        questions.insert(i);
-
-                for (char c : exprs)
-                {
-                        if (c >= '2' && c <= '9') {
-                                start = max(start, (c - '0') + 1);
-                        }
-                }
-        }
-
-        for (int i = start; i <= 9; i++)
-                bases.insert(i);
-
-
-        // 맞는 진법 찾기
-        for (int base : bases)
-        {
-                for (int i = 0; i < expressions.size(); i++)
-                {
-                        // 문제면 패스
-                        if (questions.find(i) != questions.end())
+                        //dir이 -1이 아닐 때 역 방향이라면 continue
+                        if (dir != -1 && abs(dir - i) == 2)
                                 continue;
 
-                        string& exprs = expressions[i];
-                        int a, b, result; bool isPlus;
-                        Parsing(exprs, a, b, result, isPlus);
+                        int next_x = now_x + nextdir[i].first;
+                        int next_y = now_y + nextdir[i].second;
+                        int next_cnt = (dir == i || dir == -1) ? cnt : cnt + 1; //방향이 다르다면 꺾어야만 함
 
-                        if (!Compute(a, b, result, base, isPlus))
-                        {
-                                bases.erase(base);
-                                break;
+                        if (inRange(next_x, next_y)) {
+                                //어디로 가던지 꺾은 횟수가 1번 이하여야 함
+                                if (next_cnt <= 1) {
+                                        if (boards[next_x][next_y] == '*') {
+                                                continue;
+                                        }
+                                        else if (boards[next_x][next_y] == '.') {
+                                                q.push({ next_x, next_y, i, next_cnt });
+                                        }
+                                        else {
+                                                //제거 할 수 있는 패를 찾았다면
+                                                if (next_x == end_x && next_y == end_y)
+                                                        return true;
+                                        }
+                                }
+                        }
+                }
+        }
+        return false;
+}
+
+string solve(vector<vector<ainfo>>& arr, vector<string>& boards) {
+
+        string ans = "";
+        bool flag = true;
+
+        while (flag) {
+
+                flag = false;
+
+                //2번 과정, A~Z까지 검사
+                for (int i = 0; i < arr.size(); i++) {
+                        //해결 해야할 요소가 있다면
+                        if (arr[i].size() > 1) {
+                                //BFS를 통해 제거 가능한 짝이 있는지 검사
+                                if (BFS(arr[i][0].x, arr[i][0].y, arr[i][1].x, arr[i][1].y, boards)) {
+                                        //3번 과정, 제거한 부분을 빈 공간으로 변경
+                                        boards[arr[i][0].x][arr[i][0].y] = boards[arr[i][1].x][arr[i][1].y] = '.'; //빈칸으로 만듬
+                                        arr[i].clear(); //배열 정보 삭제
+                                        pairCnt -= 2; //제거 해야할 짝의 수 감소
+                                        ans += char(i + int('A'));
+                                        flag = true;
+                                        break;
+                                }
                         }
                 }
         }
 
-        // 살아남은 진법들과, 문제들을 비교하여 풀이
-        // 모든 진법에서 같은 정답이 나오면 표기,
-        // 다른 정답이 나오면 '?'로 정답 표기
+        //4번 과정
+        if (pairCnt == 0) //모든 짝을 제거한 경우
+                return ans;
+        else //더 이상 해결을 못하는 경우
+                return "IMPOSSIBLE";
+}
 
-        vector<string> answer;
-        for (int q : questions)
-        {
-                set<int> results;
-                string& exprs = expressions[q];
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+string solution(int m, int n, vector<string> board) {
 
-                for (int base : bases)
-                {
-                        int a, b, result; bool isPlus;
-                        Parsing(exprs, a, b, result, isPlus);
-                        Compute(a, b, result, base, isPlus);
-                        results.insert(result);
+        string answer = "";
+        vector<vector<ainfo>> arr(26, vector<ainfo>());
+
+        //전역 변수 초기화
+        pairCnt = 0;
+        M = m; N = n;
+
+        //1번 과정, 모든 짝의 위치를 저장
+        for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                        if ('A' <= board[i][j] && board[i][j] <= 'Z') {
+                                //alphabet을 int로 변환 후 위치 저장
+                                arr[int(board[i][j]) - int('A')].push_back({ i,j });
+                                pairCnt++; //제거 해야 할 짝의 수 증가
+                        }
                 }
-
-                if (results.size() > 1)
-                        exprs[exprs.size() - 1] = '?';
-                else
-                {
-                        exprs.pop_back();
-                        exprs += to_string(*results.begin());
-                }
-
-                answer.push_back(exprs);
         }
 
-
-        return answer;
+        return answer = solve(arr, board);
 }
