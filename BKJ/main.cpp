c72db566f1b7ec4fa1bcc9b3d453c6506c3ecc96
Udi <dnel0605@gmail.com>
2024-10-23 15:35:17 +0900

    2024 KAKAO WINTER INTERNSHIP n + 1 게임 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,127 +1,80 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
-#include <queue>
+#include <set>
 
 using namespace std;
-int NEWNODE = 0;
 
-// - 파악 방법
-// 도넛
-// 탐색을 시작한 노드에서 출발해서 다시 돌아오는 경우
-
-// 막대
-// 특정 노드에서 출발해서 끝 지점에 도달한 경우
-
-// 8자 
-//도넛과 마찬가지로 다시 돌아오는데 중간에 입구와 출구가 2개씩 있는 애 찾기
-
-int CHECK(vector<vector<int>>& in, vector<vector<int>>& out, vector<bool>& visited, int idx)
+pair<int, int> FindCards(set<int>& deck_A, set<int>& deck_B, int cost)
 {
-        // NEW NODE를 제외한 모든 노드를 순회
-        // 모든 노드의 간선의 수가 노드의 수가 
-        // - 1    = 막대
-        // 같다면  = 도넛
-        // + 1    = 8자
-
-        int nodeCnt = 0;
-        int edgeCnt = 0;
-
-        queue<int> q;
-        q.push(idx);
-
-        while (!q.empty())
+        for (int n : deck_A)
         {
-                nodeCnt++;
-                int cur = q.front(); q.pop();
-                visited[cur] = true;
-
-                for (int next : out[cur])
-                {
-                        edgeCnt++;
-                        if (visited[next] == false)
-                                q.push(next);
-                }
-        }
-
-        // 막대 끝부분이 들어온 경우 연결된 모든 노드에 체크를 해주어야함
-
-        if (nodeCnt > edgeCnt)
-        {
-                int IDX = idx;
-                q.push(IDX);
-
-                while (!q.empty())
-                {
-                        int cur = q.front(); q.pop();
-                        visited[cur] = true;
-                        for (int next : in[cur])
-                                q.push(next);
+                int target = cost - n;
+                if (deck_B.find(target) != deck_B.end()) {
+                        deck_A.erase(n);
+                        deck_B.erase(target);
+                        return { n, target };
                 }
         }
 
 
-        // cout << idx << " : " << nodeCnt << ", " << edgeCnt << "\n";
-        return nodeCnt == edgeCnt ? 1 : nodeCnt > edgeCnt ? 2 : 3;
+        return { -1, -1 };
 }
 
-vector<int> solution(vector<vector<int>> edges) {
 
-        int size = 0;
-        for (auto& edge : edges)
-                size = max(size, max(edge[0], edge[1]));
-        size++;
+int solution(int coin, vector<int> cards) {
 
-        vector<vector<int>> in(size);
-        vector<vector<int>> out(size);
+        set<int> myDeck;
+        set<int> store;
+        int n = cards.size();
 
-        for (auto& edge : edges)
+        for (int i = 0; i < n / 3; i++)
+                myDeck.insert(cards[i]);
+
+        int round = 1;
+        for (int i = n / 3; i < n; i += 2)
         {
-                out[edge[0]].push_back(edge[1]);
-                in[edge[1]].push_back(edge[0]);
+                store.insert(cards[i]);
+                store.insert(cards[i + 1]);
 
-        }
+                // 내 손 안에 든 카드로 해결이 가능한지 체크
+                pair<int, int> cards = FindCards(myDeck, myDeck, n + 1);
+                if (cards.first == -1)
+                {
+                        // 돈 없으면 종료
+                        if (coin < 1)
+                                return round;
 
-        // 새롭게 추가된 정점 찾기
-        for (int i = 1; i < size; i++) {
-                if (out[i].size() >= in[i].size() + 2) {
-                        NEWNODE = i;
+                        // 구매 가능한 카드 + 내 카드로 해결 가능한지 체크
+                        cards = FindCards(myDeck, store, n + 1);
+                        if (cards.first == -1)
+                        {
+                                // 구매 가능한 카드들로 해결이 가능한지 체크
+                                // 돈 없거나 조합을 찾지 못했다면 종료
+                                cards = FindCards(store, store, n + 1);
+                                if (coin < 2 || cards.first == -1)
+                                        return round;
+
+                                coin -= 2;
+                        }
+                        else
+                                coin--;
                 }
+
+                round++;
         }
 
-        vector<int> answer(4);
-        answer[0] = NEWNODE;
-
-        vector<bool> visited(size);
-        for (int i = 1; i < size; i++)
-        {
-                if (i == NEWNODE || in[i].size() + out[i].size() == 0 || visited[i])
-                        continue;
-
-                answer[CHECK(in, out, visited, i)]++;
-        }
-
-        return answer;
+        return round;
 }
+// 카드 뭉치에서 n/3 뽑음 
+// 라운드별 2장 뽑음, 
+// 카드 뭉치에 남은 카드 없다면 게임 종료
 
-// 그래프의 형태 -> 도넛, 막대, 8자 모양
-// 도넛 -> 일반 통행의 형태로 한바퀴를 돌게됨
-// 막대 -> 일반 통행의 형태로 끝이 있음
-// 8자 -> 도넛과 같이 한바퀴를 돌지만 입구, 출구가 2개인 애가 하나 있음
+// 뽑은 카드는 동전 하나 소모해서 가지거나 동전 안 쓰고 버릴 수 있음
+// 카드에 적힌 수의 합이 n + 1이 되도록 카드 두 장을 내고 다음 라운드로 진행 가능
 
-// 출력 -> 추가 생성된 정점 번호, 도넛 개수, 막대 개수, 8자 개수를 출력
-// 핵심 -> 추가 생성된 정점이 뭔지 찾기, 정점 찾은 이후 그래프 파악하기
-
-// - 파악 방법
-// 도넛
-// 탐색을 시작한 노드에서 출발해서 다시 돌아오는 경우
-
-// 막대
-// 특정 노드에서 출발해서 끝 지점에 도달한 경우
-
-// 8자 
-//도넛과 마찬가지로 다시 돌아오는데 중간에 입구와 출구가 2개씩 있는 애 찾기
-
-// 추가 생성된 정점은 각 그래프와 연결되어 있음 
-// 따라서 출구 - 입구가 2개 이상인 애가 추가 생성된 정점
+// n = 12, coin = 4, cards = 3 6 7 2
+// round = 1 : 3 6 7 2 1 10, coin = 2
+// rount = 2 : 6 7 2 1 , coin = 2
+// round = 3 : 2 1 12 , coin = 1
+// round = 4 : 2 11, coin = 0
+// round = 5 : x, coin = 0 -> 종료
