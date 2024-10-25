d2edd77cee687c784daf6fd1dc6c5d5056eeda97
Udi <dnel0605@gmail.com>
2024-10-25 18:02:46 +0900

    수식 복원하기 [Lv. 3]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,60 +1,151 @@
 ﻿#include <string>
 #include <vector>
-#include <tuple>
-#include <algorithm>
-#include <map>
+#include <set>
 #include <iostream>
 
 using namespace std;
+bool Check(int a, int b, int base, bool isPlus, int& result)
+{
+        int thirdDigit = 0;
+        int secondDigit = (a / 10) + (b / 10);
+        int firstDigit = (a % 10) + (b % 10);
 
-int solution(vector<vector<int>> points, vector<vector<int>> routes) {
-        map<tuple<int, int, int>, int> logs;
-
-        for (auto& r : routes)
+        if (isPlus)
         {
-                int time = 0;
-                auto [y, x] = make_pair(points[r[0] - 1][0], points[r[0] - 1][1]);
-                logs[{time, y, x}]++;
-
-                for (int i = 1; i < r.size(); i++)
+                if (firstDigit / base > 0)
                 {
-                        auto [ry, rx] = make_pair(points[r[i] - 1][0], points[r[i] - 1][1]);
-
-                        int dy = ry > y ? 1 : ry == y ? 0 : -1;
-                        int dx = rx > x ? 1 : rx == x ? 0 : -1;
-
-                        while (y != ry)
-                        {
-                                time++;
-                                y += dy;
-                                logs[{time, y, x}]++;
-                        }
-
-                        while (x != rx)
-                        {
-                                time++;
-                                x += dx;
-                                logs[{time, y, x}]++;
-                        }
+                        secondDigit++;
+                        firstDigit -= base;
                 }
 
-
-
+                if (secondDigit / base > 0)
+                {
+                        thirdDigit++;
+                        secondDigit -= base;
+                }
+        }
+        else
+        {
+                secondDigit = (a / 10) - (b / 10); // 무조건 0보다 큼
+                firstDigit = (a % 10) - (b % 10);
+                if (firstDigit < 0) {
+                        secondDigit--;
+                        firstDigit = (firstDigit % base + base) % base;
+                }
         }
 
-        int answer = 0;
-        for (auto& log : logs)
+        int total = thirdDigit * 100 + secondDigit * 10 + firstDigit;
+
+        if (result == -1)
+                result = total;
+
+        return total == result;
+}
+
+bool check(string exp, int base, int& result)
+{
+        int idx = 0;
+        int a = exp[idx] - '0';
+        if (exp[idx + 1] != ' ')
         {
-                //       cout << get<0>(log.first) << " : " <<  get<1>(log.first) << ", " << get<2>(log.first) << " -> " << log.second << "\n";
-                if (log.second > 1)
-                        answer++;
+                a *= 10;
+                a += exp[++idx] - '0';
+        }
+        idx += 2;
+
+        bool isPlus = exp[idx] == '+' ? true : false;
+
+        idx += 2;
+
+        int b = exp[idx] - '0';
+        if (exp[idx + 1] != ' ')
+        {
+                b *= 10;
+                b += exp[++idx] - '0';
+        }
+
+        idx += 4;
+
+        result = exp[idx] - '0';
+        if (exp[idx] == 'X')
+                result = -1;
+        idx++;
+        while (idx < exp.size())
+        {
+                result = result * 10 + exp[idx++] - '0';
+        }
+
+
+        //    string test = isPlus ? "+" : "-";
+         //  cout << "\nEXP : " << exp << "\n";
+        //    cout << "Phasing : " << a << " " << test << " " << b << " = " << result << "\n";
+        return Check(a, b, base, isPlus, result);
+}
+
+
+vector<string> solution(vector<string> expressions) {
+
+        set<int> bases;
+        for (int i = 0; i <= 9; i++)
+                bases.insert(i);
+
+        int bigNum = 0;
+        for (string exp : expressions)
+                for (char& c : exp)
+                        if (c >= '0' && c <= '9')
+                                bigNum = max(bigNum, c - '0');
+
+        for (int i = bigNum; i >= 0; i--)
+                bases.erase(i);
+
+
+        for (int base : bases)
+        {
+                for (string& exp : expressions)
+                {
+                        if (exp.back() == 'X')
+                                continue;
+
+                        int temp = 0;
+                        if (!check(exp, base, temp))
+                        {
+                                bases.erase(base);
+                                break;
+                        }
+                }
+        }
+
+
+        vector<string> answer;
+        for (string& exp : expressions)
+        {
+                if (exp[exp.size() - 1] != 'X')
+                        continue;
+
+                set<int> rets;
+                for (int base : bases)
+                {
+                        int ret = 0;
+                        check(exp, base, ret);
+                        rets.insert(ret);
+                }
+
+                exp.pop_back();
+                if (rets.size() == 1)
+                        exp += to_string(*rets.begin());
+                else
+                        exp += '?';
+
+
+                answer.push_back(exp);
         }
 
         return answer;
 }
 
-// 같은 시간에 2개 이상의 로봇이 겹친 횟수를 구해야함
-// 시간별로 위치를 기록
-// 시간을 기준으로 오름차순 정렬
-// 같은 시간에 같은 위치 2개 이상인 횟수 기록
-// 100 * 100 * 100 => 최대 100만 (가능)
+// 여러 수식을 입력 받을 때
+// 해당 수식들이 몇진법으로 만들어진 것인지 찾아야함 (2~ 9진법)
+// 우선 수식들 중 가장 큰 수를 찾고 해당 수 보다 작은 수는 가능한 진법 목록에서 삭제
+// 살아 남은 진법들을 이용해서 수식 풀이 -> 불가능 하다면 삭제
+// 마지막 까지 살아남은 진법들로 문제 풀이 -> 정답이 하나로 동일하면 해당 수를 입력, 
+// 정답이 여러개가 나오면 ?를 입력
