7d3d4a7b41c8821dbce3bc24f621f47c730465e4
Udi <dnel0605@gmail.com>
2024-10-25 13:41:17 +0900

    붕대 감기 [Lv. 1]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,23 +1,44 @@
 ﻿#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-int solution(int n, vector<int> tops) {
-        int answer = 0;
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 3 + tops[0];
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
 
+        int hp = health;
+        int healCnt = 0;
 
-        for (int i = 2; i <= n; i++)
+        int time = 0;
+        int idx = 0;
+        while (idx < attacks.size())
         {
-                dp[i] = (dp[i - 1] * (3 + tops[i - 1]) - dp[i - 2]) % 10007;
+                // 공격 받을 시간이 됐다면 ?
+                if (attacks[idx][0] == time)
+                {
+                        hp -= attacks[idx++][1];
+                        healCnt = 0;
+                        if (hp <= 0)
+                                return -1;
+                }
+                else
+                {
+                        hp += bandage[1];
+                        if (++healCnt == bandage[0])
+                        {
+                                healCnt = 0;
+                                hp += bandage[2];
+                        }
+                        hp = min(hp, health);
+                }
 
-                if (dp[i] < 0)
-                        dp[i] += 10007;
+                //  cout << "TIME : " << time << " IDX : " << idx << " HP : " << hp <<"\n";
+                time++;
         }
-        return dp[n];
+
+        return hp;
 }
 
-// n = 1
+// bandage = [시전 시간, 초당 회복량, 추가 회복량(시전 시간 채웠을 경우))]
+// health = 최대 체력
+// attacks = [[공격 받는 시간, 피해량], ... ... ... ]
