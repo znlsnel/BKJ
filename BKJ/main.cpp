473bc727623d2928c7f30da51d2f3d16b13b0f03
Udi <dnel0605@gmail.com>
2024-10-29 12:12:25 +0900

    교점에 별 만들기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,32 +1,52 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
+#include <set>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
-int solution(int n) {
-        if (n % 2 == 1)
-                return 0;
+vector<string> solution(vector<vector<int>> line) {
+        vector<string> answer;
+        set<pair<long long, long long>> pos;
 
-        vector<long long> dp(n + 1);
+        for (int i = 0; i < line.size() - 1; i++) {
+                for (int j = i + 1; j < line.size(); j++) {
+                        long long AD_BC = (long long)line[i][0] * line[j][1] - (long long)line[i][1] * line[j][0];
+                        if (AD_BC != 0) {
+                                double x = ((double)line[i][1] * line[j][2] - (double)line[i][2] * line[j][1]) / AD_BC;
+                                double y = ((double)line[i][2] * line[j][0] - (double)line[i][0] * line[j][2]) / AD_BC;
 
-        dp[0] = 1;
-        dp[2] = 3;
-        dp[4] = 11;
-
-        for (int i = 6; i <= n; i += 2)
-        {
-                dp[i] = dp[i - 2] * 3;
-                for (int j = i - 4; j >= 0; j -= 2)
-                        dp[i] += dp[j] * 2;
-
-                dp[i] %= 1000000007;
+                                if (x - (long long)x == 0 && y - (long long)y == 0) {
+                                        pos.insert({ x, y });
+                                }
+                        }
+                }
         }
 
-        return dp[n];
+        long long min_x = 9223372036854775807;
+        long long max_x = -9223372036854775808;
+        long long min_y = 9223372036854775807;
+        long long max_y = -9223372036854775808;
+
+        for (const auto& p : pos) {
+                min_x = min(p.first, min_x);
+                min_y = min(p.second, min_y);
+                max_x = max(p.first, max_x);
+                max_y = max(p.second, max_y);
+        }
+
+        for (long long y = min_y; y <= max_y; y++) {
+                string tmp = "";
+                for (long long x = min_x; x <= max_x; x++) {
+                        tmp += ".";
+                }
+                answer.push_back(tmp);
+        }
+
+        for (const auto& p : pos) {
+                answer[abs(p.second - max_y)][abs(p.first - min_x)] = '*';
+        }
+
+        return answer;
 }
-// 연결 -> 4칸 -> 2개 * 
-// 0 = 1;
-// 2 = 3
-// 4 = 11
-// 6 = 33 6 2
