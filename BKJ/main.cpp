8a6466076b4c4d71fe58c1051fe5bf290b748628
Udi <dnel0605@gmail.com>
2024-11-01 15:14:13 +0900

    조이스틱 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,29 +1,19 @@
-﻿#include <string>
-#include <vector>
-
+﻿#include<bits/stdc++.h>
 using namespace std;
 
-int CheckPrime(long long num)
-{
-        int result = 1;
-        for (int i = 2; i * i <= num; i++)
-        {
-                if (num % i == 0) {
-                        if (num / i <= 10000000)
-                                return num / i;
+int solution(string name) {
+        int alphaMove[26] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1 };
+        int ans = 0, n = name.size();
+        int minMove = n - 1;
+        for (int x = 0; x < n; x++) {
+                ans += alphaMove[name[x] - 'A'];
+                int y = x + 1; // x 오른쪽에 있으면서 A가 아닌 문자가 있는 위치를 y라하자
 
-                        result = i;
-                }
+                while (y < n && name[y] == 'A')
+                        y++;
 
+                minMove = min(minMove, min(x + x + (n - y), x + (n - y) + (n - y)));
         }
-        return num == 1 ? 0 : result;
-}
-
-vector<int> solution(long long begin, long long end) {
-        vector<int> answer;
-
-        for (long long i = begin; i <= end; i++)
-                answer.push_back(CheckPrime(i));
-
-        return answer;
+        ans += minMove;
+        return ans;
 }
