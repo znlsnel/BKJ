c451d6f98b9e604a469397b4b8336db865c3f5a8
Udi <dnel0605@gmail.com>
2024-11-15 22:49:28 +0900

    멀쩡한 사각형 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,43 +1,14 @@
-﻿#include <string>
-#include <iostream>
+﻿using namespace std;
 
-using namespace std;
+long long solution(int w, int h) {
+        long long answer = 0;
 
-int solution(string s) {
-
-        if (s.size() == 1)
-                return 1;
-
-        int answer = 99999999;
-
-        for (int len = 1; len <= s.size() / 2; len++)
+        for (int i = 1; i < w; i++)
         {
-                string str = s.substr(0, len);
-                string tmp = "";
-                int cnt = 1;
 
-                int idx = 0;
-                for (int i = len; i < s.size(); i += len)
-                {
-                        idx = i;
-                        string nxt = s.substr(i, len);
-
-                        if (str == nxt)
-                                cnt++;
-
-                        else
-                        {
-                                tmp = cnt > 1 ? tmp + to_string(cnt) + str : tmp + str;
-                                cnt = 1;
-                                str = nxt;
-                        }
-                }
-
-                str = s.substr(idx, len);
-                tmp = cnt > 1 ? tmp + to_string(cnt) + str : tmp + str;
-
-                answer = min(answer, int(tmp.size()));
+                long long n = ((double)h * i) / w;
+                answer += n;
         }
-
+        answer *= 2;
         return answer;
 }
