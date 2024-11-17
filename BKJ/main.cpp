8c32054c81a68e63930edf4121c20d925265111a
Udi <dnel0605@gmail.com>
2024-11-17 15:33:39 +0900

    가장 큰 정사각형 찾기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,58 +1,34 @@
-﻿#include <string>
-#include <vector>
-#include <iostream>
-
+﻿#include <iostream>
+#include<vector>
 using namespace std;
 
-int answer = 999999999;
-void DFS(vector<string>& minerals, int dia, int iron, int stone, int cost = 0, int idx = 0)
+int solution(vector<vector<int>> board)
 {
-        if (idx >= minerals.size() || dia + iron + stone == 0)
+        int answer = 0;
+
+        for (auto& b : board)
+                for (int& n : b)
+                        answer = n == 1 ? 1 : n;
+
+
+        for (int i = 1; i < board.size(); i++)
         {
-                answer = min(answer, cost);
-                return;
-        }
-
-        int left = min(int(minerals.size()) - idx, 5);
-
-        // 다이아몬드 곡괭이를 사용할 때
-        if (dia > 0)
-                DFS(minerals, dia - 1, iron, stone, cost + left, idx + left);
-
-        // 철 곡괭이를 사용할 때
-        if (iron > 0)
-        {
-                int nxtCost = 0;
-                for (int i = idx; i < idx + left; i++)
+                for (int j = 1; j < board[i].size(); j++)
                 {
-                        if (minerals[i] == "diamond")
-                                nxtCost += 5;
-                        else
-                                nxtCost += 1;
+                        if (board[i][j] == 0)
+                                continue;
+
+                        int left = board[i][j - 1];
+                        int up = board[i - 1][j];
+                        int dia = board[i - 1][j - 1];
+
+                        board[i][j] = min(left, min(up, dia)) + 1;
+                        answer = max(answer, board[i][j] * board[i][j]);
                 }
-                DFS(minerals, dia, iron - 1, stone, cost + nxtCost, idx + left);
+
         }
 
 
-        // 돌 곡괭이 사용할 때
-        if (stone > 0)
-        {
-                int nxtCost = 0;
-                for (int i = idx; i < idx + left; i++)
-                {
-                        if (minerals[i] == "diamond")
-                                nxtCost += 25;
-                        else if (minerals[i] == "iron")
-                                nxtCost += 5;
-                        else
-                                nxtCost += 1;
-                }
-                DFS(minerals, dia, iron, stone - 1, cost + nxtCost, idx + left);
-        }
 
-}
-
-int solution(vector<int> picks, vector<string> minerals) {
-        DFS(minerals, picks[0], picks[1], picks[2]);
         return answer;
 }
