926cb4b929a395bddbe7846fefa051f417b33f96
Udi <dnel0605@gmail.com>
2024-11-18 11:11:49 +0900

    거리두기 확인하기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,34 +1,61 @@
-﻿#include <iostream>
-#include<vector>
+﻿#include <string>
+#include <vector>
+
 using namespace std;
 
-int solution(vector<vector<int>> board)
+bool CHECK(vector<string>& places, vector<pair<int, int>>& P)
 {
-        int answer = 0;
-
-        for (auto& b : board)
-                for (int& n : b)
-                        answer = n == 1 ? 1 : n;
-
-
-        for (int i = 1; i < board.size(); i++)
+        for (int i = 0; i < P.size(); i++)
         {
-                for (int j = 1; j < board[i].size(); j++)
+                for (int j = i + 1; j < P.size(); j++)
                 {
-                        if (board[i][j] == 0)
-                                continue;
+                        auto& [cy, cx] = P[i];
+                        auto& [ty, tx] = P[j];
 
-                        int left = board[i][j - 1];
-                        int up = board[i - 1][j];
-                        int dia = board[i - 1][j - 1];
+                        int dist = abs(cy - ty) + abs(cx - tx);
+                        if (dist == 1)
+                                return false;
 
-                        board[i][j] = min(left, min(up, dia)) + 1;
-                        answer = max(answer, board[i][j] * board[i][j]);
+                        if (dist == 2)
+                        {
+                                if (cy == ty && cx != tx && places[cy][(cx + tx) / 2] != 'X')
+                                        return false;
+                                if (cx == tx && cy != ty && places[(cy + ty) / 2][cx] != 'X')
+                                        return false;
+                                if (cy != ty && cx != tx)
+                                {
+                                        if (places[cy][tx] != 'X' || places[ty][cx] != 'X')
+                                                return false;
+                                }
+                        }
                 }
-
         }
 
+        return true;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+        vector<int> answer(places.size());
+
+        vector<vector<pair<int, int>>> PPos(places.size(), vector<pair<int, int>>());
+        for (int N = 0; N < places.size(); N++)
+        {
+                auto& place = places[N];
+                for (int i = 0; i < place.size(); i++)
+                        for (int j = 0; j < place[i].size(); j++)
+                                if (place[i][j] == 'P')
+                                        PPos[N].push_back({ i, j });
+        }
+
+        for (int i = 0; i < PPos.size(); i++)
+                answer[i] = CHECK(places[i], PPos[i]) ? 1 : 0;
 
 
         return answer;
 }
+
+// POOOP
+// OXXOX
+// OPXPX
+// OOXOX
+// POXXP
