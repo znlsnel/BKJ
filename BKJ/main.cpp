762beb3274b1b7ef11282a9f9cf6c7fea4017dac
Udi <dnel0605@gmail.com>
2024-11-19 16:45:58 +0900

    하노이의 탑 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,61 +1,31 @@
 ﻿#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-bool CHECK(vector<string>& places, vector<pair<int, int>>& P)
+vector<vector<int>> answer;
+void hanoi(int n, int from, int by, int to)
 {
-        for (int i = 0; i < P.size(); i++)
+        if (n == 1)
         {
-                for (int j = i + 1; j < P.size(); j++)
-                {
-                        auto& [cy, cx] = P[i];
-                        auto& [ty, tx] = P[j];
-
-                        int dist = abs(cy - ty) + abs(cx - tx);
-                        if (dist == 1)
-                                return false;
-
-                        if (dist == 2)
-                        {
-                                if (cy == ty && cx != tx && places[cy][(cx + tx) / 2] != 'X')
-                                        return false;
-                                if (cx == tx && cy != ty && places[(cy + ty) / 2][cx] != 'X')
-                                        return false;
-                                if (cy != ty && cx != tx)
-                                {
-                                        if (places[cy][tx] != 'X' || places[ty][cx] != 'X')
-                                                return false;
-                                }
-                        }
-                }
+                answer.push_back({ from, to });
+                cout << from << ", " << to << "\n";
         }
+        else
+        {
+                hanoi(n - 1, from, to, by);
 
-        return true;
+                answer.push_back({ from, to });
+                cout << from << ", " << to << "\n";
+
+                hanoi(n - 1, by, from, to);
+        }
 }
 
-vector<int> solution(vector<vector<string>> places) {
-        vector<int> answer(places.size());
+vector<vector<int>> solution(int n) {
 
-        vector<vector<pair<int, int>>> PPos(places.size(), vector<pair<int, int>>());
-        for (int N = 0; N < places.size(); N++)
-        {
-                auto& place = places[N];
-                for (int i = 0; i < place.size(); i++)
-                        for (int j = 0; j < place[i].size(); j++)
-                                if (place[i][j] == 'P')
-                                        PPos[N].push_back({ i, j });
-        }
-
-        for (int i = 0; i < PPos.size(); i++)
-                answer[i] = CHECK(places[i], PPos[i]) ? 1 : 0;
-
+        hanoi(n, 1, 2, 3);
 
         return answer;
 }
-
-// POOOP
-// OXXOX
-// OPXPX
-// OOXOX
-// POXXP
