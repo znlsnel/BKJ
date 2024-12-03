081e08d1915c06a170d909855487af18eed0aac3
Udi <dnel0605@gmail.com>
2024-12-03 15:27:52 +0900

    연속된 부분 수열의 합 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,74 +1,40 @@
 ﻿#include <string>
 #include <vector>
-#include <deque>
-#include <iostream>
 
 using namespace std;
 
-vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
-        vector<int> answer;
+vector<int> solution(vector<int> sequence, int k) {
+        int s = 0, e = 0;
+        int sum = sequence[0]; // 부분 수열의 합
+        int subLen = sequence.size() + 1; // 부분 수열의 길이
+        pair<int, int> result; // 부분 수열의 시작 인덱스와 마지막 인덱스를 담은 객체
 
-        vector<vector<int>> table(100, vector<int>(100));
-        for (int i = 0; i < 100; i++)
-                for (int j = 0; j < 100; j++)
-                        table[i][j] = i * columns + j + 1;
-
-        for (auto& querie : queries)
+        while (s <= e && e < sequence.size())
         {
-                pair<int, int> start = { querie[0] - 1, querie[1] - 1 };
-                pair<int, int> end = { querie[2] - 1, querie[3] - 1 };
+                if (sum < k)
+                        sum += sequence[++e];
 
-                int value = 99999999;
-
-                deque<int> dq;
-                vector<pair<int, int>> pos;
-
-                for (int i = start.second; i <= end.second; i++)
+                else if (sum == k)
                 {
-                        dq.push_back(table[start.first][i]);
-                        pos.push_back({ start.first, i });
+                        if (e - s + 1 < subLen)
+                        { // 길이가 더 짧은 수열이면
+                                subLen = e - s + 1;
+                                result = { s, e };
+                        }
+
+                        else if (e - s + 1 == subLen)
+                        {
+                                if (s < result.first)
+                                { // 시작 인덱스가 더 작으면
+                                        result = { s, e };
+                                }
+                        }
+
+                        sum -= sequence[s++];
                 }
 
-                for (int i = start.first + 1; i <= end.first; i++)
-                {
-                        dq.push_back(table[i][end.second]);
-                        pos.push_back({ i, end.second });
-
-                }
-
-                for (int i = end.second - 1; i >= start.second; i--)
-                {
-                        dq.push_back(table[end.first][i]);
-                        pos.push_back({ end.first, i });
-                }
-
-
-                for (int i = end.first - 1; i >= start.first + 1; i--)
-                {
-                        dq.push_back(table[i][start.second]);
-                        pos.push_back({ i, start.second });
-                }
-
-                dq.push_front(dq.back());
-                dq.pop_back();
-
-
-                for (int i = 0; i < dq.size(); i++)
-                {
-                        table[pos[i].first][pos[i].second] = dq[i];
-                        //     cout << dq[i] << " ";
-                        value = min(value, dq[i]);
-                }
-                //     cout << "\n";
-                start.first++;
-                start.second++;
-
-                end.first--;
-                end.second--;
-
-                answer.push_back(value);
+                else sum -= sequence[s++];
         }
 
-
-        return answer;
+        return { result.first, result.second };
 }
