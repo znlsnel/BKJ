7401bdd7c486c29fdc9027bd61bd97cb2fb465a3
Udi <dnel0605@gmail.com>
2024-12-29 23:15:12 +0900

    호텔 대실 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,39 +1,43 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
-#include <queue>
+
 using namespace std;
 
-int solution(int n) {
+int ttoi(string time, bool isEnd = false)
+{
+        int ret = isEnd ? 9 : 0;
+        ret += time[4] - '0';
+        ret += (time[3] - '0') * 10;
+        ret += (time[1] - '0') * 60;
+        ret += (time[0] - '0') * 600;
+        return ret;
+}
+
+int solution(vector<vector<string>> book_time) {
         int answer = 0;
+        vector<int> dp(9999);
 
-        queue<int> q;
-        int total = 0;
-        int num = 1;
-
-        while (num <= n + 1)
+        for (auto& book : book_time)
         {
-                if (total >= n)
-                {
-                        //      cout << "pop " + to_string(q.front()) << "\n";
-                        total -= q.front();
-                        q.pop();
-                }
-                else
-                {
-                        //     cout << "push " + to_string(num) << "\n";
-                        total += num;
-                        q.push(num++);
-                }
+                int start = ttoi(book[0]);
+                int end = ttoi(book[1], true);
 
+                dp[start]++;
+                dp[end + 1]--;
 
-                if (total == n)
-                {
-                        answer++;
-                        //    cout << "찾았다\n\n";
-                }
-
+                // while (start <= end)
+                // {
+                //     dp[start]++;
+              //       answer = max(answer, dp[start]);
+               //      start++;
+               //  }
         }
+        answer = dp[0];
+        for (int i = 1; i < 9999; i++) {
+                dp[i] = dp[i] + dp[i - 1];
+                answer = max(answer, dp[i]);
+        }
+
 
         return answer;
 }
