b52ae73c34c269d35ec54a99ecffbe82db8f0973
Udi <dnel0605@gmail.com>
2025-01-05 21:21:15 +0900

    마법의 엘리베이터 {Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,36 +1,20 @@
-﻿#include <string>
-#include <vector>
+﻿#include<algorithm>
 
 using namespace std;
 
-long long solution(vector<int> weights) {
-        long long answer = 0;
-        vector<long long> cnt(1001, 0); //몸무게 별 개수
+int GetCost(int num)
+{
+        if (num < 10)
+                return min(num, 10 - num + 1);
 
-        for (int i = 0; i < weights.size(); i++)
-                cnt[weights[i]]++;
+        int A = num % 10;
+        int B = 10 - num % 10;
 
-        for (int i = 0; i < weights.size(); i++) {
-                //2:3
-                if (weights[i] % 2 == 0) {
-                        long long base = (weights[i] / 2) * 3;
-                        if (base <= 1000)    answer += cnt[base];
-                }
-                //3:4
-                if (weights[i] % 3 == 0) {
-                        long long base = (weights[i] / 3) * 4;
-                        if (base <= 1000)    answer += cnt[base];
-                }
-                //1:2
-                long long base = weights[i] * 2;
-                if (base <= 1000)    answer += cnt[base];
-        }
+        return min(GetCost((num - A) / 10) + A, GetCost((num + B) / 10) + B);
+}
 
-        /* 몸무게 같은 경우 처리 */
-        for (int i = 100; i <= 1000; i++) {
-                if (cnt[i] >= 2)
-                        answer += (long long)(cnt[i] * (cnt[i] - 1)) / 2; //n개 중에 2개 뽑는 경우의 수 : n(n-1) / 2
-        }
+int solution(int storey)
+{
 
-        return answer;
+        return GetCost(storey);
 }
