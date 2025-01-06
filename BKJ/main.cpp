9727c1619c62172385a1d84285c2bd3687cc14c4
Udi <dnel0605@gmail.com>
2025-01-06 14:23:36 +0900

    귤 고르기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,20 +1,40 @@
-﻿#include<algorithm>
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <map>
 
 using namespace std;
 
-int GetCost(int num)
-{
-        if (num < 10)
-                return min(num, 10 - num + 1);
+int solution(int k, vector<int> tangerine) {
 
-        int A = num % 10;
-        int B = 10 - num % 10;
 
-        return min(GetCost((num - A) / 10) + A, GetCost((num + B) / 10) + B);
+        map<int, int> t;
+        for (int n : tangerine)
+                t[n]++;
+
+        vector<pair<int, int>> v;
+        for (auto p : t)
+                v.push_back(p);
+
+        sort(v.begin(), v.end(),
+                [](pair<int, int> a, pair<int, int> b) {return a.second > b.second; });
+
+        int answer = 0;
+        int total = 0;
+        for (auto p : v)
+        {
+                total += p.second;
+                answer++;
+                if (total >= k)
+                        break;
+        }
+
+        return answer;
 }
 
-int solution(int storey)
-{
 
-        return GetCost(storey);
-}
+// 서로 다른 크기의 종류 최소화
+// 1 3 2 5 4 5 2 3
+// 정렬
+// 1 2 2 3 3 4 5 5
