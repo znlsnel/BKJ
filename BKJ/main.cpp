dcc26ffc5aaa885a31f9c82c34e76147e0607e44
Udi <dnel0605@gmail.com>
2025-01-08 12:49:55 +0900

    롤케이크 자르기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,39 +1,29 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
+#include <map>
+
 using namespace std;
-int gcd(int a, int b)
-{
-        if (b == 0)
-                return a;
 
-        return gcd(b, a % b);
-}
+int solution(vector<int> topping) {
 
-int solution(vector<int> arrayA, vector<int> arrayB)
-{
-        int gcdA = arrayA[0];
-        int gcdB = arrayB[0];
+        map<int, int> left, right;
+        for (int i = 0; i < topping.size(); i++)
+                right[topping[i]]++;
 
-        for (int n : arrayA)
-                gcdA = gcd(gcdA, n);
-
-        for (int n : arrayB)
-                gcdB = gcd(gcdB, n);
-
-        for (int i = 0; i < arrayA.size(); i++)
+        int answer = 0;
+        for (int i = 0; i < topping.size(); i++)
         {
-                if (arrayA[i] % gcdB == 0)
-                        gcdB = -1;
+                int t = topping[i];
+                left[t]++;
+
+                right[t]--;
+                if (right[t] <= 0)
+                        right.erase(t);
+
+                if (left.size() == right.size())
+                        answer++;
         }
 
-        for (int i = 0; i < arrayB.size(); i++)
-        {
-                if (arrayB[i] % gcdA == 0)
-                        gcdA = -1;
-        }
 
-        int answer = max(gcdA, gcdB);
-
-        return max(answer, 0);
+        return answer;
 }
