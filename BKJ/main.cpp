36f15003829157d255b2afcd60faa9a0d37f4cc1
Udi <dnel0605@gmail.com>
2025-01-12 16:11:09 +0900

    k진수에서 소수 개수 구하기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,51 +1,97 @@
 ﻿#include <string>
 #include <vector>
-#include <map>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int solution(vector<string> want, vector<int> number, vector<string> discount)
+string convert(int n, int k)
 {
-        int answer = 0;
-        map<string, int> w, d;
+        string ret;
+        while (n > 0)
+        {
+                ret = to_string(n % k) + ret;
+                n /= k;
+        }
+        return ret;
+}
 
-        for (int i = 0; i < want.size(); i++) {
-                w.insert({ want[i], number[i] });
-                d.insert({ want[i], 0 });
+bool isPrime(long long num) {
+        if (num < 2) return false;
+        for (long long i = 2; i * i <= num; i++) {
+                if (num % i == 0) return false;
+        }
+        return true;
+}
+
+bool isPrime(string num)
+{
+        long long ret = 0;
+        for (int i = 0; i < num.size(); i++)
+        {
+                ret *= 10;
+                ret += num[i] - '0';
         }
 
-        int left = 0; int right = 10;
-        for (int i = 0; i < discount.size() && i < 10; i++)
-                d[discount[i]]++;
+        return isPrime(ret);
+}
 
+int solution(int n, int k)
+{
+        vector<int> zeros;
 
-        while (true)
+        string number = convert(n, k);
+
+        for (int i = 0; i < number.size(); i++)
+                if (number[i] == '0')
+                        zeros.push_back(i);
+
+        int answer = 0;
+        if (zeros.size() == 0 && isPrime(number))
+                answer++;
+
+        int prev = -1;
+        for (int i = 0; i <= zeros.size(); i++)
         {
-                bool find = true;
-                for (auto p : w)
-                {
-                        string name = p.first;
-                        int cnt = p.second;
-
-                        if (d[name] < cnt)
-                        {
-                                find = false;
-                                break;
-                        }
+                if (zeros.size() == 0) {
+                        break;
                 }
-                if (find)
+
+                int idx = 0;
+                if (i < zeros.size())
+                        idx = zeros[i];
+                string num = "";
+                if (prev == -1)
+                        num = number.substr(0, idx);
+
+                else if (i == zeros.size() && zeros.size() > 0)
+                        num = number.substr(prev + 1, number.size() - prev);
+
+                else
+                        num = number.substr(prev + 1, idx - (prev + 1));
+
+                if (num != "" && isPrime(num))
                         answer++;
 
-                if (left >= discount.size())
-                        break;
-
-
-                d[discount[left++]]--;
-                if (right < discount.size())
-                        d[discount[right++]]++;
+                prev = idx;
         }
-
 
 
         return answer;
 }
+
+// 소수 찾기~
+// vector<int> ~ ~ ~ ~ ~ ~ ~
+
+// n -> 소수 ? answer++;
+// n 을 k진수로 변환 ( string 형식으로 보관 )
+// 오른쪽에 0이 있는 경우
+// 왼쪽에 0이 있는 경우
+// 양쪽에 0이 있는 경우
+
+// 0의 위치를 vector로 저장 (오름차순)
+// [22] 0 [2] 0 [2] 0 0 [22] 0 [2] 0 [2] 0
+
+// 오른쪽 먼저
+// vector<int> 0모음 에서 index가 0 ~ N 까지 탐색 시작
+// i를 탐색 할때 
