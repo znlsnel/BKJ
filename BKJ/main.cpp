446916fcc6a6eb840026d9ff7613a70a4f6f7783
Udi <dnel0605@gmail.com>
2025-01-15 13:04:21 +0900

    전력망을 둘로 나누기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,18 +1,61 @@
 ﻿#include <string>
 #include <vector>
-
+#include <map>
+#include <iostream>
 using namespace std;
 
-vector<int> solution(int n, long long left, long long right)
+int find(vector<int>& parents, int a)
 {
-        vector<int> answer;
-        while (left <= right)
+        if (a == parents[a])
+                return a;
+
+        return parents[a] = find(parents, parents[a]);
+}
+
+void Union(vector<int>& parents, int a, int b)
+{
+        a = find(parents, a);
+        b = find(parents, b);
+
+        if (a <= b)
+                parents[b] = a;
+        else
+                parents[a] = b;
+}
+
+
+
+int solution(int n, vector<vector<int>> wires)
+{
+        int answer = 100000000;
+
+        for (int i = 0; i < wires.size(); i++)
         {
-                int y = left / n;
-                int x = left % n;
-                answer.push_back(max(y, x) + 1);
-                left++;
+                vector<int> parents(n);
+                for (int j = 0; j < parents.size(); j++)
+                        parents[j] = j;
+
+                map<int, int> m;
+                for (int j = 0; j < wires.size(); j++)
+                {
+                        if (j == i)
+                                continue;
+
+                        int a = wires[j][0] - 1;
+                        int b = wires[j][1] - 1;
+
+                        Union(parents, a, b);
+                }
+                for (int n : parents)
+                        m[find(parents, n)]++;
+
+                vector<int> idxs;
+                for (auto a : m)
+                        idxs.push_back(a.first);
+
+                answer = min(answer, abs(m[idxs[0]] - m[idxs[1]]));
         }
+
 
         return answer;
 }
