6b000044f9ee2980eadab36720c35eb19c3e1f8e
Udi <dnel0605@gmail.com>
2025-01-18 13:45:09 +0900

    괄호 회전하기 [Lv. 2]

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,74 +1,55 @@
 ﻿#include <string>
 #include <vector>
+#include <map>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-string itob(long long num)
+bool check(string& str)
 {
-        string ret = "";
-        while (num > 0)
+        stack<char> stk;
+        for (char c : str)
         {
+                if (c == '(' || c == '[' || c == '{')
+                        stk.push(c);
 
-                ret = to_string(num % 2) + ret;
-                num /= 2;
-        }
-
-        return ret;
-}
-
-long long btoi(string num)
-{
-        long long ret = 0;
-
-        for (int i = 0; i < num.size(); i++)
-        {
-                ret *= 2;
-                ret += num[i] - '0';
-        }
-        return ret;
-
-
-}
-
-vector<long long> solution(vector<long long> numbers) {
-        vector<long long> answer;
-
-        for (auto num : numbers)
-        {
-                string bit = itob(num);
-
-                // bit중에 0이 있다면 가장 오른쪽의 0을 1로 바꾼 수 (1개 차이)
-                // bit중에 0이 없다면 가장 왼쪽의 0을 1로 바꾸고 그 다음 1을 0으로 바꿈 (2개 차이)
-
-                int idx = -1;
-                for (int i = bit.size() - 1; i >= 0; i--)
-                {
-                        if (bit[i] == '0')
-                        {
-                                idx = i;
-                                break;
-                        }
-                }
-
-                // 1개로 해결 가능
-                if (idx != -1)
-                {
-                        bit[idx] = '1';
-                        if (idx < bit.size() - 1)
-                                bit[idx + 1] = '0';
-                }
                 else
                 {
-                        bit[0] = '0';
-                        bit = "1" + bit;
+                        // 스택이 비어 있거나 짝이 맞지 않으면 false
+                        if (stk.empty())
+                                return false;
+
+                        char top = stk.top();
+                        if ((c == ')' && top == '(') ||
+                                (c == ']' && top == '[') ||
+                                (c == '}' && top == '{'))
+                        {
+                                stk.pop(); // 짝이 맞으면 스택에서 제거
+                        }
+
+                        else
+                                return false; // 짝이 맞지 않음
                 }
-                answer.push_back(btoi(bit));
-
-
         }
+
+        // 모든 괄호가 짝지어져야 유효
+        return stk.empty();
+}
+
+int solution(string s)
+{
+        int answer = 0;
+        int cnt = s.size();
+
+        while (cnt--)
+        {
+                if (check(s))
+                        answer++;
+
+                s = s.substr(1, s.size() - 1) + s[0];
+        }
+
 
         return answer;
 }
-//  11111
-// 101111
