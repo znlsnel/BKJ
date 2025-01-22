17d1d07d53ebf1f1cb05537db58cd4cf6c8e9e9f
Udi <dnel0605@gmail.com>
2025-01-22 16:48:08 +0900

    다음 큰 숫자 (C#)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,28 +1,60 @@
-﻿#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+﻿using System;
 
-using namespace std;
-
-
-int solution(vector<int> people, int limit)
+class Solution
 {
-        sort(people.begin(), people.end());
-        int answer = 0;
-        int idx = 0;
-
-
-        while (idx < people.size())
+        char[] itob(int num)
         {
-                int back = people.back();
-                people.pop_back();
+                string str = "";
+                while (num > 0)
+                {
+                        str = (num % 2) + str;
+                        num /= 2;
+                }
 
-                if (back + people[idx] <= limit)
-                        idx++;
+                char[] ret = new char[str.Length];
+                for (int i = 0; i < str.Length; i++)
+                        ret[i] = str[i];
 
-                answer++;
+                return ret;
         }
 
-        return answer;
+        int btoi(char[] num)
+        {
+                int ret = 0;
+
+                for (int i = num.Length - 1; i >= 0; i--)
+                {
+                        ret *= 2;
+                        ret += num[i] - '0';
+                }
+                return ret;
+        }
+
+        public int solution(int n)
+        {
+                int answer = 0;
+                char[] bit = itob(n);
+
+                int cnt = 0;
+                for (int i = 0; i < bit.Length; i++)
+                {
+                        if (bit[i] == '1')
+                                cnt++;
+                }
+
+                while (true)
+                {
+                        bit = itob(++n);
+                        int cur = 0;
+                        for (int i = 0; i < bit.Length; i++)
+                                if (bit[i] == '1')
+                                        cur++;
+
+                        if (cur == cnt)
+                                break;
+                }
+
+
+                return n;
+        }
 }
