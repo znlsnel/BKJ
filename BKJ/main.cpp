b89e7fe5af47deb711cc14c3b76b886de87824a2
Udi <dnel0605@gmail.com>
2025-01-24 20:13:09 +0900

    피보나치 수 (C#)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,60 +1,14 @@
-﻿using System;
-
-class Solution
-{
-        char[] itob(int num)
-        {
-                string str = "";
-                while (num > 0)
-                {
-                        str = (num % 2) + str;
-                        num /= 2;
-                }
-
-                char[] ret = new char[str.Length];
-                for (int i = 0; i < str.Length; i++)
-                        ret[i] = str[i];
-
-                return ret;
-        }
-
-        int btoi(char[] num)
-        {
-                int ret = 0;
-
-                for (int i = num.Length - 1; i >= 0; i--)
-                {
-                        ret *= 2;
-                        ret += num[i] - '0';
-                }
-                return ret;
-        }
-
-        public int solution(int n)
-        {
+﻿public class Solution {
+        public int solution(int n) {
                 int answer = 0;
-                char[] bit = itob(n);
+                int[] dp = new int[n + 1];
 
-                int cnt = 0;
-                for (int i = 0; i < bit.Length; i++)
-                {
-                        if (bit[i] == '1')
-                                cnt++;
-                }
+                dp[0] = 0;
+                dp[1] = 1;
 
-                while (true)
-                {
-                        bit = itob(++n);
-                        int cur = 0;
-                        for (int i = 0; i < bit.Length; i++)
-                                if (bit[i] == '1')
-                                        cur++;
+                for (int i = 2; i <= n; i++)
+                        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
 
-                        if (cur == cnt)
-                                break;
-                }
-
-
-                return n;
+                return dp[n];
         }
 }
