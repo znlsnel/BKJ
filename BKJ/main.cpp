4c791b2aefdcfe048a0bf815d3cf02f6f41441f9
Udi <dnel0605@gmail.com>
2025-01-29 12:09:39 +0900

    메뉴 리뉴얼 (C++)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,44 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
 
-public class Solution {
-        public int[] solution(int[] progresses, int[] speeds) {
+using namespace std;
 
-
-                int[] times = new int[speeds.Length];
-                for (int i = 0; i < progresses.Length; i++) {
-                        times[i] = (100 - progresses[i]) / speeds[i];
-                        if ((100 - progresses[i]) % speeds[i] > 0)
-                                times[i]++;
-                }
-
-
-                List<int> ans = new List<int>();
-
-                int maxNum = times[0];
-                int cnt = 1;
-                for (int i = 1; i < times.Length; i++)
-                {
-                        if (times[i] > maxNum)
-                        {
-                                maxNum = times[i];
-                                ans.Add(cnt);
-                                cnt = 1;
-                        }
-                        else
-                                cnt++;
-
-                }
-                ans.Add(cnt);
-
-                int[] answer = new int[ans.Count];
-                for (int i = 0; i < ans.Count; i++)
-                        answer[i] = ans[i];
-
-
-                return answer;
+void GetMenuCombi(vector<string>& result, string& order, int num, string str = "", int idx = 0)
+{
+        if (str.size() == num)
+        {
+                result.push_back(str);
+                return;
         }
+
+        for (int i = idx; i < order.size(); i++)
+        {
+                GetMenuCombi(result, order, num, str + order[i], i + 1);
+        }
+
 }
 
-// 5 10 1 1 1 20
-// v v        v
+vector<string> solution(vector<string> orders, vector<int> course) {
+
+        map<string, int> combiCnt;
+
+        for (string order : orders)
+        {
+                sort(order.begin(), order.end());
+                for (int n : course)
+                {
+                        vector<string> combis;
+                        GetMenuCombi(combis, order, n);
+
+                        for (string combi : combis)
+                                combiCnt[combi]++;
+                }
+        }
+
+        map<int, pair<int, vector<string>>> ans;
+        for (auto c : combiCnt)
+        {
+                string str = c.first;
+                int len = c.first.size();
+                int cnt = c.second;
+
+                if (cnt == 1)
+                        continue;
+
+                if (ans[len].first < cnt)
+                {
+                        ans[len].first = cnt;
+                        ans[len].second.clear();
+                }
+
+                if (ans[len].first == cnt)
+                        ans[len].second.push_back(str);
+        }
+
+        vector<string> answer;
+
+        for (auto [len, p] : ans)
+        {
+                for (string str : p.second)
+                        answer.push_back(str);
+        }
+
+        sort(answer.begin(), answer.end());
+        return answer;
+}
+
+
+// orders.size <= 20
+// course.size <= 10 (2 <= value <= 10)
+
+// orders[idx] = 2;
+// orders 1234 = 12 13 14 23 24 34
+
+// orders[idx] = 3;
+// orders 1234 = 123 124 14 23 24 34
