998ce05645e6b3b02fec077d0d08393ffe8b89ab
Udi <dnel0605@gmail.com>
2025-01-30 21:56:22 +0900

    방문 길이 (C++)

--- BKJ/main.cpp
+++ BKJ/main.cpp
@@ -1,81 +1,51 @@
 ﻿#include <string>
-#include <vector>
+#include <set>
 #include <iostream>
-#include <map>
-#include <algorithm>
 
 using namespace std;
 
-void GetMenuCombi(vector<string>& result, string& order, int num, string str = "", int idx = 0)
+int clamp(int t, int a, int b)
 {
-        if (str.size() == num)
-        {
-                result.push_back(str);
-                return;
-        }
+        if (t < a)
+                t = a;
+        else if (t > b)
+                t = b;
 
-        for (int i = idx; i < order.size(); i++)
-        {
-                GetMenuCombi(result, order, num, str + order[i], i + 1);
-        }
-
+        return t;
 }
+int solution(string dirs) {
+        int answer = 0;
 
-vector<string> solution(vector<string> orders, vector<int> course) {
+        set<pair<int, int>> roads;
+        int cy = 0, cx = 0;
 
-        map<string, int> combiCnt;
-
-        for (string order : orders)
+        for (char dir : dirs)
         {
-                sort(order.begin(), order.end());
-                for (int n : course)
-                {
-                        vector<string> combis;
-                        GetMenuCombi(combis, order, n);
+                auto [ny, nx] = make_pair(cy, cx);
 
-                        for (string combi : combis)
-                                combiCnt[combi]++;
-                }
+                if (dir == 'U')
+                        ny += 1;
+                else if (dir == 'D')
+                        ny -= 1;
+                else if (dir == 'R')
+                        nx += 1;
+                else if (dir == 'L')
+                        nx -= 1;
+
+                ny = clamp(ny, -5, 5);
+                nx = clamp(nx, -5, 5);
+
+                int idxC = cy * 11 + cx;
+                int idxN = ny * 11 + nx;
+
+                if (idxC != idxN)
+                        roads.insert({ min(idxC, idxN), max(idxC, idxN) });
+
+                cy = ny;
+                cx = nx;
         }
 
-        map<int, pair<int, vector<string>>> ans;
-        for (auto c : combiCnt)
-        {
-                string str = c.first;
-                int len = c.first.size();
-                int cnt = c.second;
 
-                if (cnt == 1)
-                        continue;
 
-                if (ans[len].first < cnt)
-                {
-                        ans[len].first = cnt;
-                        ans[len].second.clear();
-                }
-
-                if (ans[len].first == cnt)
-                        ans[len].second.push_back(str);
-        }
-
-        vector<string> answer;
-
-        for (auto [len, p] : ans)
-        {
-                for (string str : p.second)
-                        answer.push_back(str);
-        }
-
-        sort(answer.begin(), answer.end());
-        return answer;
+        return roads.size();
 }
-
-
-// orders.size <= 20
-// course.size <= 10 (2 <= value <= 10)
-
-// orders[idx] = 2;
-// orders 1234 = 12 13 14 23 24 34
-
-// orders[idx] = 3;
-// orders 1234 = 123 124 14 23 24 34
